2ee6f1b813c12b227854b65afab647b9f778ce22
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-04-22 08:45:31 -0500

    4-22-2022 Added production servers, moved base html template, added user-conditional login/logout nav links, added user & timestamp function to safety checklist submission

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(
--- app/core/admin.py
+++ app/core/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from .models import userprofile
 from .models import checklistlog
+from .models import Sample
 
-admin.site.register(checklistlog)
-
-@admin.register(userprofile)
-class userprofileAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone_number')
-    ordering = ('last_name',)
-
-from core.models import Sample
-
+@admin.register(checklistlog)
+class checkListLogAdmin(admin.ModelAdmin):
+    list_display=('date', 'operator_name', 'unit_number')
+    ordering=('-date',)
 admin.site.register(Sample)
