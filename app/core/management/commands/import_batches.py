48730f42f4e848e56c9abdc2cccfdbe45c96e947
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-05-09 10:08:19 -0500

    clear lotnumexcel residue

--- app/core/management/commands/import_batches.py
+++ app/core/management/commands/import_batches.py
@@ -1,6 +1,7 @@
 import csv
 from django.core.management import BaseCommand
 from core.models import lotnumrecord
+from decimal import Decimal
 
 class Command(BaseCommand):
     help = 'Load a lotnumbers csv file into the database'
@@ -12,11 +13,17 @@ class Command(BaseCommand):
         path = kwargs['path']
         with open(path, 'rt') as f:
             reader = csv.reader(f, dialect='excel')
+            next(reader)
+            next(reader)
             for row in reader:
+                if row[3]:
+                    rowAtThree=row[3]
+                else:
+                    rowAtThree=Decimal(0)
                 lotnumrecord.objects.create(
                     part_number=row[0],
                     description=row[1],
                     lot_number=row[2],
-                    quantity=row[3],
+                    quantity=rowAtThree,
                     date=row[4],
                 )
