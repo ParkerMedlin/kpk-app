43567c77739f825f1f7074ef4737bc4673503a18
kpkappserver <jdavis@kinpakinc.com>
2022-06-14 14:31:45 -0500

    updates to the scripts

--- scripts/batch-scripts/ProdMergeToPostgr.bat
+++ scripts/batch-scripts/ProdMergeToPostgr.bat
@@ -1,1 +1,6 @@
+@echo off
+set LOGFILE=batch.log
+call :LOG > %LOGFILE%
+exit /B
+:LOG
 "C:\Users\Blendverse\AppData\Local\Programs\Python\Python310\python.exe" "C:\users\Blendverse\Documents\kpk-app\scripts\direct-db-table-scripts\ProdMergetoPostgres.py"
