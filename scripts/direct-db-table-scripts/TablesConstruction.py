115e2854e3f1740d5cf631c4343041f7cd8e225b
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-06-15 11:25:55 -0500

    added billdesc to bill_of_materials for real this time

--- scripts/direct-db-table-scripts/TablesConstruction.py
+++ scripts/direct-db-table-scripts/TablesConstruction.py
@@ -14,7 +14,6 @@ bomcursorPG = cnxnPG.cursor()
 bomcursorPG.execute('drop table if exists bill_of_materials')
 bomcursorPG.execute('''CREATE TABLE bill_of_materials as
                         select distinct Bm_BillDetail.billno AS bill_pn,
-                            bm_billheader.billdesc1 as bill_desc,
                             ci_item.itemcode as component_itemcode,
                             ci_item.itemcodedesc as component_desc,
                             ci_item.procurementtype as procurementtype,
@@ -36,6 +35,8 @@ bomcursorPG.execute('''CREATE TABLE bill_of_materials as
                         )
 bomcursorPG.execute('alter table bill_of_materials add hundred_gx smallint;')
 bomcursorPG.execute('alter table bill_of_materials add adjusted_qtyonhand numeric;')
+bomcursorPG.execute('alter table bill_of_materials add bill_desc text;')
+bomcursorPG.execute("update bill_of_materials set bill_desc=(select ci_item.itemcodedesc from ci_item where bill_of_materials.bill_pn=ci_item.itemcode);")
 bomcursorPG.execute("update bill_of_materials set hundred_gx=100 where standard_uom='100G';")
 bomcursorPG.execute("update bill_of_materials set hundred_gx=1 where standard_uom!='100G';")
 bomcursorPG.execute("update bill_of_materials set adjusted_qtyonhand=hundred_gx*unadjusted_qtyonhand;")
