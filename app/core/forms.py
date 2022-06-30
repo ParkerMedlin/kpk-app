7200efb4ea6d5c721d295b912ed8f1049fde2160
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-06-30 14:29:05 -0500

    lost in space

--- app/core/forms.py
+++ app/core/forms.py
@@ -20,24 +20,22 @@ class ReportForm(forms.Form):
 # Form for Django-created input table checklistlog.html
 class ChecklistLogForm(forms.ModelForm):
     # Set all checkboxes to require user to check the box
-    engine_oil_checked = forms.BooleanField(required=True)
-    propane_tank_checked = forms.BooleanField(required=True)
-    radiator_leaks_checked = forms.BooleanField(required=True)
-    tires_checked = forms.BooleanField(required=True)
-    mast_forks_checked = forms.BooleanField(required=True)
-    leaks_checked = forms.BooleanField(required=True)
-    horn_checked = forms.BooleanField(required=True)
-    driver_compartment_checked = forms.BooleanField(required=True)
-    seatbelt_checked = forms.BooleanField(required=True)
-    battery_checked = forms.BooleanField(required=True)
-    safety_equipment_checked = forms.BooleanField(required=True)
-    steering_checked = forms.BooleanField(required=True)
-    brakes_checked = forms.BooleanField(required=True)
-    unit_number = forms.ModelChoiceField(queryset=Forklift.objects.all(), to_field_name = 'forklift_id', empty_label="Select Forklift Number")
-    
+    engine_oil_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('False', 'Bad')), widget=forms.RadioSelect)
+    propane_tank_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    radiator_leaks_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    tires_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    mast_forks_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    leaks_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    horn_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    driver_compartment_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    seatbelt_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    battery_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    safety_equipment_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    steering_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+    brakes_checked = forms.ChoiceField(required=True, choices=(('Good', 'Good'), ('Bad', 'Bad')), widget=forms.RadioSelect)
+
     class Meta:
         model = ChecklistLog
-        
         fields = (
                     'unit_number',
                     'serial_number',
@@ -68,10 +66,15 @@ class ChecklistLogForm(forms.ModelForm):
                     'brakes_checked',
                     'brakes_comments'
                     )
-                
+        labels = {
+                    'radiator_leaks_comments': 'Radiator comments',
+                    'mast_forks_checked': 'Mast and forks checked',
+                    'mast_forks_comments': 'Mast and forks comments',
+                }
         widgets = {
             'date': forms.HiddenInput(),
             'operator_name': forms.HiddenInput(),
+            'engine_oil_checked': forms.RadioSelect(),
             'engine_oil_comments': forms.Textarea(attrs={'cols':'23', 'rows':'2'}),
             'propane_tank_comments': forms.Textarea(attrs={'cols':'23', 'rows':'2'}),
             'radiator_leaks_comments': forms.Textarea(attrs={'cols':'23', 'rows':'2'}),
@@ -87,15 +90,26 @@ class ChecklistLogForm(forms.ModelForm):
             'brakes_comments': forms.Textarea(attrs={'cols':'23', 'rows':'2'}, ),
         }
 
-        def clean(self):
-            shipping = self.cleaned_data.get('engine_oil_checked')
-            if shipping:
-                msg = forms.ValidationError("This field is required.")
-                self.add_error('engine_oil_comments', msg)
-            else:
-                # Keep the database consistent. The user may have
-                # submitted a shipping_destination even if shipping
-                # was not selected
-                self.cleaned_data['engine_oil_comments'] = 'BEEEEP'
+    def fields_required(self, fields):
+    #Used for conditionally marking fields as required.
+        for field in fields:
+            if not self.cleaned_data.get(field, ''):
+                print('we are now setting the field to required yeehaw')
+                print('the value of reqfield = ' + self.cleaned_data.get(field))
+                msg = forms.ValidationError("Tell us what's wrong.")
+                self.add_error(field, msg)
 
-            return self.cleaned_data
+    def clean(self):
+        checkfieldlist = ['engine_oil_checked', 'propane_tank_checked', 'radiator_leaks_checked', 'tires_checked', 'mast_forks_checked', 'leaks_checked', 'horn_checked', 'driver_compartment_checked', 'seatbelt_checked', 'battery_checked', 'safety_equipment_checked', 'steering_checked', 'brakes_checked']
+        reqfieldlist = ['engine_oil_comments', 'propane_tank_comments', 'radiator_leaks_comments', 'tires_comments', 'mast_forks_comments', 'leaks_comments', 'horn_comments', 'driver_compartment_comments', 'seatbelt_comments', 'battery_comments', 'safety_equipment_comments', 'steering_comments', 'brakes_comments']
+        for checkfield, reqfield in zip(checkfieldlist, reqfieldlist):
+            mrclean_data = self.cleaned_data.get(checkfield)
+            print('the value of mrclean_data = ' + mrclean_data)
+            if mrclean_data == 'False':
+                print('mrclean_data returned false and we are here')
+                print(reqfield)
+                self.fields_required([reqfield])
+            else:
+                self.cleaned_data[reqfield] = ''
+            continue
+        return self.cleaned_data
