6219719878b2bc8e76f0c781bbe63a14b6b67773
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-06-30 15:01:09 -0500

    hiding secret

--- scripts/forkliftemailer.py
+++ scripts/forkliftemailer.py
@@ -1,10 +1,10 @@
 from O365 import Account
 
-credentials = ('6318854d-eeff-454e-bf84-5f529f147fba', 'Drn8Q~Q6Ynx25seZQMKjJwG-GQAQebNBR_e9yacl')
+credentials = ('client_id', 'client_secret')
 
 account = Account(credentials)
 m = account.new_message()
-m.to.add('jdavis@kinpakinc.com')
-m.subject = 'Sent from forkliftemailer.py'
-m.body = "Uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+m.to.add('to_example@example.com')
+m.subject = 'Testing!'
+m.body = "George Best quote: I've stopped drinking, but only while I'm asleep."
 m.send()
