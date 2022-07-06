ac60b4f36bb31d47cbf5acbe13aac8bf769b5740
kpkappserver <jdavis@kinpakinc.com>
2022-07-06 05:39:21 -0500

    added rudimentary error handling to datalooper.py

--- scripts/direct-db-table-scripts/datalooper.py
+++ scripts/direct-db-table-scripts/datalooper.py
@@ -2,7 +2,18 @@ import time
 import ProdMergetoPostgres as fProdMerge
 import TablesConstruction as fTables
 
-while(True):
-    fProdMerge.GetLatestProdMerge()
-    fTables.BuildTables()
-    print('oh boy here I go again')
+for retries in range(100):
+    for attempt in range(10):
+        try:
+            while(True):
+                fProdMerge.GetLatestProdMerge()
+                fTables.BuildTables()
+                print('oh boy here I go again')
+        except:
+            print("well well well, looks like we need to take a breaky wakey")
+            time.sleep(10)
+        else:
+            break
+    else:
+        print("we should try taking a longer break, gonna wait for 1 minute then try again")
+        time.sleep(60)
