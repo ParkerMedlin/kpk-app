8d22dbd4a5f91ad6ccef0f8c61a6eae898a25110
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-07-11 07:20:36 -0500

    added user setup batches to one-time-Tabler.py

--- scripts/direct-db-table-scripts/one-time-Tabler.py
+++ scripts/direct-db-table-scripts/one-time-Tabler.py
@@ -12,3 +12,5 @@ fTables.BuildTables()
 fChemLoc.GetChemLocations()
 
 subprocess.call([r'.\scripts\batch-scripts\importsAnd_d-c-UP.bat'])
+
+subprocess.call([r'.\scripts\batch-scripts\userSetup.bat'])
