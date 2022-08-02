bec23c59cb0e22ac1b600a7ca307882d0fb019dc
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-08-02 08:22:51 -0500

    err handle comit

--- scripts/direct-db-table-scripts/one-time-Tabler.py
+++ scripts/direct-db-table-scripts/one-time-Tabler.py
@@ -7,10 +7,10 @@ import subprocess
 tblList = ['BM_BillHeader', 'BM_BillDetail', 'CI_Item', 'IM_ItemWarehouse', 'IM_ItemCost', 'IM_ItemTransactionHistory', 'PO_PurchaseOrderDetail']
 for item in tblList:
     fSage.GetSageTable(item)
-fProdMerge.GetLatestProdMerge()
-fTables.BuildTables()
-fChemLoc.GetChemLocations()
+# fProdMerge.GetLatestProdMerge()
+# fTables.BuildTables()
+# fChemLoc.GetChemLocations()
 
-subprocess.call([r'.\scripts\batch-scripts\importsAnd_d-c-UP.bat'])
+# subprocess.call([r'.\scripts\batch-scripts\importsAnd_d-c-UP.bat'])
 
-subprocess.call([r'.\scripts\batch-scripts\userSetup.bat'])
+# subprocess.call([r'.\scripts\batch-scripts\userSetup.bat'])
