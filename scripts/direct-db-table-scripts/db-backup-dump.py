a00964671ab9dabb69b043d14e310bdb2acd3f9b
BLENDVERSE SERVER <me@parkermedl.in>
2022-08-25 13:00:55 -0500

    Add print feedback to db backup script

--- scripts/direct-db-table-scripts/db-backup-dump.py
+++ scripts/direct-db-table-scripts/db-backup-dump.py
@@ -4,4 +4,5 @@ import time
 
 while(True):
     subprocess.call([r'C:\Users\pmedl\Documents\kpk-app\scripts\batch-scripts\db-backup.bat'])
+    print('Database dumped. Sleeping for 2 hours then taking another dump')
     time.sleep(7200)
