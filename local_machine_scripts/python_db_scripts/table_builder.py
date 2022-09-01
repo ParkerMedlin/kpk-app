2c2b410383ecb986408e28c7bc77231de8b686b2
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-09-01 14:16:55 -0500

    Update table construction function.

--- local_machine_scripts/python_db_scripts/table_builder.py
+++ local_machine_scripts/python_db_scripts/table_builder.py
@@ -7,7 +7,9 @@ from sqlalchemy import create_engine
 def create_tables():
     print('create_tables():')
     time_start = time.perf_counter()
-    connection_postgres = psycopg2.connect('postgresql://postgres:blend2021@localhost:5432/blendversedb')
+    connection_postgres = psycopg2.connect(
+        'postgresql://postgres:blend2021@localhost:5432/blendversedb'
+        )
     cursor_postgres = connection_postgres.cursor()
     cursor_postgres.execute('''CREATE TABLE blend_bill_of_materials_TEMP as
                             select distinct Bm_BillDetail.billno AS bill_pn,
@@ -22,7 +24,9 @@ def create_tables():
                             FROM ci_item AS ci_item
                             JOIN Bm_BillDetail Bm_BillDetail ON ci_item.itemcode=Bm_BillDetail.componentitemcode
                             left join core_foamfactor core_foamfactor on ci_item.itemcode=core_foamfactor.blend
-                            left join im_itemwarehouse im_itemwarehouse on ci_item.itemcode=im_itemwarehouse.itemcode and im_itemwarehouse.warehousecode = 'MTG'
+                            left join im_itemwarehouse im_itemwarehouse 
+                                on ci_item.itemcode=im_itemwarehouse.itemcode 
+                                and im_itemwarehouse.warehousecode = 'MTG'
                             left join bm_billheader bm_billheader on ci_item.itemcode=bm_billheader.billno
                             where ci_item.itemcodedesc like 'CHEM -%' 
                                 or ci_item.itemcodedesc like 'BLEND-%' 
@@ -30,19 +34,25 @@ def create_tables():
                                 or ci_item.itemcodedesc like 'DYE%'
                             order by bill_pn'''
                             )
-    cursor_postgres.execute('alter table blend_bill_of_materials_TEMP add id serial primary key;')                        
+    cursor_postgres.execute('alter table blend_bill_of_materials_TEMP add id serial primary key;')
     cursor_postgres.execute('alter table blend_bill_of_materials_TEMP add bill_desc text;')
-    cursor_postgres.execute('update blend_bill_of_materials_TEMP set bill_desc=(select ci_item.itemcodedesc from ci_item where blend_bill_of_materials_TEMP.bill_pn=ci_item.itemcode);')
-    cursor_postgres.execute("update blend_bill_of_materials_TEMP set foam_factor=1 where foam_factor IS NULL;")
+    cursor_postgres.execute('''update blend_bill_of_materials_TEMP set bill_desc=
+                                (select ci_item.itemcodedesc from ci_item 
+                                where blend_bill_of_materials_TEMP.bill_pn=ci_item.itemcode);''')
+    cursor_postgres.execute('''update blend_bill_of_materials_TEMP
+                                set foam_factor=1 where foam_factor IS NULL;''')
     cursor_postgres.execute('drop table if exists blend_bill_of_materials')
-    cursor_postgres.execute('alter table blend_bill_of_materials_TEMP rename to blend_bill_of_materials')
+    cursor_postgres.execute('''alter table blend_bill_of_materials_TEMP
+                                rename to blend_bill_of_materials''')
     cursor_postgres.execute('drop table if exists blend_bill_of_materials_TEMP')
     connection_postgres.commit()
     cursor_postgres.close()
     print('blend_bill_of_materials table created')
 
 
-    connection_postgres = psycopg2.connect('postgresql://postgres:blend2021@localhost:5432/blendversedb')
+    connection_postgres = psycopg2.connect(
+        'postgresql://postgres:blend2021@localhost:5432/blendversedb'
+        )
     cursor_postgres = connection_postgres.cursor()
     cursor_postgres.execute('''CREATE TABLE prod_bill_of_materials_TEMP as
                             SELECT BM_BillDetail.BillNo, 
@@ -54,16 +64,20 @@ def create_tables():
                             CI_Item.ProcurementType, 
                             CI_Item.StandardUnitofMeasure, 
                             BM_BillDetail.CommentText 
-                            FROM BM_BillDetail BM_BillDetail, BM_BillHeader BM_BillHeader, CI_Item CI_Item
+                            FROM BM_BillDetail BM_BillDetail, 
+                                BM_BillHeader BM_BillHeader, CI_Item CI_Item
                             WHERE BM_BillDetail.BillNo = BM_BillHeader.BillNo 
                             AND BM_BillDetail.Revision = BM_BillHeader.Revision 
                             AND BM_BillDetail.ComponentItemCode = CI_Item.ItemCode 
                             AND ((CI_Item.ItemCodeDesc Not Like '...%') 
                             AND (BM_BillDetail.ComponentItemCode Not Like '/__%'))'''
                             )
-    cursor_postgres.execute("update prod_bill_of_materials_TEMP set ItemCodeDesc=CommentText where ItemCodeDesc='Default Item Code /C'")
+    cursor_postgres.execute("""update prod_bill_of_materials_TEMP
+                            set ItemCodeDesc=CommentText 
+                            where ItemCodeDesc='Default Item Code /C'""")
     cursor_postgres.execute('drop table if exists prod_bill_of_materials')
-    cursor_postgres.execute('alter table prod_bill_of_materials_TEMP rename to prod_bill_of_materials')
+    cursor_postgres.execute('''alter table prod_bill_of_materials_TEMP
+                                rename to prod_bill_of_materials''')
     cursor_postgres.execute('drop table if exists prod_bill_of_materials_TEMP')
     connection_postgres.commit()
     cursor_postgres.close()
@@ -84,12 +98,15 @@ def create_tables():
                                 prodmerge_run_data.prodline as prodline,
                                 prodmerge_run_data.id2 as id2
                             from prodmerge_run_data as prodmerge_run_data
-                            join blend_bill_of_materials blend_bill_of_materials on prodmerge_run_data.p_n=blend_bill_of_materials.bill_pn and procurementtype='M'
+                            join blend_bill_of_materials blend_bill_of_materials 
+                                on prodmerge_run_data.p_n=blend_bill_of_materials.bill_pn 
+                                and procurementtype='M'
                             order by starttime'''
                             )
     cursor_postgres.execute('alter table blend_run_data_TEMP add id serial primary key;')
     cursor_postgres.execute('alter table blend_run_data_TEMP add adjustedrunqty numeric;')
-    cursor_postgres.execute('update blend_run_data_TEMP set adjustedrunqty=(unadjusted_runqty*1.1*foam_factor*qtyperbill)')
+    cursor_postgres.execute('''update blend_run_data_TEMP
+                            set adjustedrunqty=(unadjusted_runqty*1.1*foam_factor*qtyperbill)''')
     cursor_postgres.execute('drop table if exists blend_run_data')
     cursor_postgres.execute('alter table blend_run_data_TEMP rename to blend_run_data')
     cursor_postgres.execute('drop table if exists blend_run_data_TEMP')
@@ -126,7 +143,8 @@ def create_tables():
                             select * from timetable_run_data where starttime < 20
                             order by prodline, starttime'''
                             )
-    cursor_postgres.execute('''alter table issue_sheet_needed_TEMP add batchnum1 text, add batchqty1 text, 
+    cursor_postgres.execute('''alter table issue_sheet_needed_TEMP
+                            add batchnum1 text, add batchqty1 text,
                             add batchnum2 text, add batchqty2 text,
                             add batchnum3 text, add batchqty3 text,
                             add batchnum4 text, add batchqty4 text,
@@ -148,30 +166,52 @@ def create_tables():
         blend_pn_list.append(blend_tuple[0])
     cursor_postgres = connection_postgres.cursor()
     for blend_pn in blend_pn_list:
-        cursor_postgres.execute("select receiptno, quantityonhand from im_itemcost where itemcode='" + blend_pn + "'and quantityonhand!=0 and receiptno ~ '^[A-Z].*$' order by receiptdate")
+        cursor_postgres.execute("select sum(quantityonhand) from im_itemcost where itemcode='"
+                                + blend_pn
+                                + "' and quantityonhand!=0 and receiptno !~ '^[A-Z].*$'")
+        for item in cursor_postgres:
+            non_standard_lot_total = item[0]
+        if non_standard_lot_total is None:
+            non_standard_lot_total = 0
+        cursor_postgres.execute("select receiptno, quantityonhand from im_itemcost where itemcode='"
+                                + blend_pn
+                                + "'and quantityonhand!=0 and receiptno ~ '^[A-Z].*$' order by receiptdate")
         connection_postgres.commit()
         batch_tuples = cursor_postgres.fetchall()
         batch_num_list = ['n/a','n/a','n/a','n/a','n/a','n/a','n/a','n/a','n/a']
-        batch_qty_list = ['n/a','n/a','n/a','n/a','n/a','n/a','n/a','n/a','n/a']
+        batch_qty_list = [0,'n/a','n/a','n/a','n/a','n/a','n/a','n/a','n/a']
         list_pos = 0
         for this_tuple in batch_tuples:
             batch_num_list[list_pos] = this_tuple[0]
             batch_qty_list[list_pos] = str(round(this_tuple[1],0))
             list_pos += 1
+        batch_qty_list[0] = str(float(batch_qty_list[0]) + float(non_standard_lot_total))
         item_number = 1
         batch_num = 'batchnum'
         batch_qty = 'batchqty'
         for item_number in range(9):
             batch_num+=str(item_number+1)
             batch_qty+=str(item_number+1)
-            cursor_postgres.execute("update issue_sheet_needed_TEMP set " + batch_num + "='" + batch_num_list[item_number] + "' where blend_pn='" + blend_pn + "'")
-            cursor_postgres.execute("update issue_sheet_needed_TEMP set " + batch_qty + "='" + batch_qty_list[item_number] + "' where blend_pn='" + blend_pn + "'")
+            cursor_postgres.execute("update issue_sheet_needed_TEMP set "
+                                    + batch_num
+                                    + "='"
+                                    + batch_num_list[item_number]
+                                    + "' where blend_pn='"
+                                    + blend_pn
+                                    + "'")
+            cursor_postgres.execute("update issue_sheet_needed_TEMP set "
+                                    + batch_qty
+                                    + "='"
+                                    + batch_qty_list[item_number]
+                                    + "' where blend_pn='"
+                                    + blend_pn
+                                    + "'")
             batch_num = 'batchnum'
             batch_qty = 'batchqty'
     connection_postgres.commit()
     cursor_postgres.close()
     cursor_postgres = connection_postgres.cursor()
-    cursor_postgres.execute('''update issue_sheet_needed_TEMP 
+    cursor_postgres.execute('''update issue_sheet_needed_TEMP
                             set uniqchek=concat(prodline, blend_pn)''')
     cursor_postgres.execute('''DELETE FROM issue_sheet_needed_TEMP a USING issue_sheet_needed_TEMP b
                                 WHERE a.id > b.id AND a.uniqchek = b.uniqchek;''')
@@ -181,7 +221,6 @@ def create_tables():
     connection_postgres.commit()
     cursor_postgres.close()
     print('issue_sheet_needed table created')
-    
 
     cursor_postgres = connection_postgres.cursor()
     cursor_postgres.execute('drop table if exists blendthese_TEMP')
@@ -200,11 +239,16 @@ def create_tables():
     cursor_postgres.execute('select distinct blendthese_TEMP.blend_pn from blendthese_TEMP')
     tuple_list = cursor_postgres.fetchall()
     part_number_list = []
-    for tuple in tuple_list:
-        part_number_list.append(tuple[0])
-    alchemy_engine = create_engine('postgresql+psycopg2://postgres:blend2021@localhost:5432/blendversedb', pool_recycle=3600)
+    for this_tuple in tuple_list:
+        part_number_list.append(this_tuple[0])
+    alchemy_engine = create_engine(
+        'postgresql+psycopg2://postgres:blend2021@localhost:5432/blendversedb',
+        pool_recycle=3600
+        )
     alchemy_connection = alchemy_engine.connect()
-    timetable_df = pd.read_sql('select blend_pn, adjustedrunqty, oh_after_run, week_calc from timetable_run_data where oh_after_run<0', alchemy_connection)
+    timetable_df = pd.read_sql('''select blend_pn, adjustedrunqty, oh_after_run, week_calc
+                                from timetable_run_data where oh_after_run<0''', alchemy_connection
+                                )
     pd.set_option('display.expand_frame_repr', False)
     one_week_df = timetable_df[timetable_df.week_calc.isin([2.0,3.0]) == False]
     two_week_df = timetable_df[timetable_df.week_calc.isin([3.0]) == False]
@@ -215,21 +259,41 @@ def create_tables():
             qty_this_blend_one_week = 0
         else:
             qty_this_blend_one_week =  filtered_one_week_df.iloc[-1,2] * -1
-        cursor_postgres.execute("update blendthese_TEMP set one_wk_short=" + "'" + str(qty_this_blend_one_week) + "'" + " where blend_pn=" + "'" + part_number + "'")
+        cursor_postgres.execute("update blendthese_TEMP set one_wk_short="
+                                + "'"
+                                + str(qty_this_blend_one_week)
+                                + "'"
+                                + " where blend_pn="
+                                + "'"
+                                + part_number
+                                + "'")
 
         filtered_two_week_df = two_week_df.loc[two_week_df['blend_pn'] == part_number]
         if len(filtered_two_week_df) == 0:
             qty_this_blend_two_weeks = 0
         else:
             qty_this_blend_two_weeks =  filtered_two_week_df.iloc[-1,2] * -1
-        cursor_postgres.execute("update blendthese_TEMP set two_wk_short=" + "'" + str(qty_this_blend_two_weeks) + "'" + " where blend_pn=" + "'" + part_number + "'")
+        cursor_postgres.execute("update blendthese_TEMP set two_wk_short="
+                                + "'"
+                                + str(qty_this_blend_two_weeks)
+                                + "'"
+                                + " where blend_pn="
+                                + "'"
+                                + part_number
+                                + "'")
 
         filtered_three_week_df = three_week_df.loc[three_week_df['blend_pn'] == part_number]
         if len(filtered_three_week_df) == 0:
             qty_this_blend_three_weeks = 0
         else:
             qty_this_blend_three_weeks = filtered_three_week_df.iloc[-1,2] * -1
-        cursor_postgres.execute("update blendthese_TEMP set three_wk_short=" + "'" + str(qty_this_blend_three_weeks)+"'" + " where blend_pn=" + "'" + part_number + "'")
+        cursor_postgres.execute("update blendthese_TEMP set three_wk_short="
+                                + "'"
+                                + str(qty_this_blend_three_weeks)+"'"
+                                + " where blend_pn="
+                                + "'"
+                                + part_number
+                                + "'")
 
     cursor_postgres.execute('drop table if exists blendthese')
     cursor_postgres.execute('alter table blendthese_TEMP rename to blendthese')
@@ -249,8 +313,10 @@ def create_tables():
                                     timetable_run_data.prodline as prodline
                                 from timetable_run_data as timetable_run_data 
                                 ''')
-    cursor_postgres.execute('alter table upcoming_blend_count_TEMP add column id serial primary key')
-    cursor_postgres.execute('''DELETE FROM upcoming_blend_count_TEMP a USING upcoming_blend_count_TEMP b
+    cursor_postgres.execute('''alter table upcoming_blend_count_TEMP
+                            add column id serial primary key''')
+    cursor_postgres.execute('''DELETE FROM upcoming_blend_count_TEMP a
+                                USING upcoming_blend_count_TEMP b
                                 WHERE a.id > b.id AND a.blend_pn = b.blend_pn;''')
     cursor_postgres.execute('drop table if exists upcoming_blend_count')
     cursor_postgres.execute('alter table upcoming_blend_count_TEMP rename to upcoming_blend_count')
@@ -261,4 +327,5 @@ def create_tables():
 
     connection_postgres.close()
     time_checkpoint = time.perf_counter()
-    print(f'Complete in {time_checkpoint - time_start:0.4f} seconds','world record prolly')
+    print(f'Complete in {time_checkpoint - time_start:0.4f} seconds',
+            'world record prolly')
