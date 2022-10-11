48232d0cb683f96b19f1b598988f4702260fbf7f
BLENDVERSE SERVER <me@parkermedl.in>
2022-10-11 07:40:50 -0500

    Refine error handling in the monitor script.

--- local_machine_scripts/python_db_scripts/db_backup_dump.py
+++ local_machine_scripts/python_db_scripts/db_backup_dump.py
@@ -2,6 +2,6 @@ import subprocess
 import time
 
 while True:
-    subprocess.call([r'C:\Users\pmedl\Documents\kpk-app\scripts\batch-scripts\db_backup.bat'])
+    subprocess.call([r'C:\Users\pmedl\Documents\kpk-app\local_machine_scripts\batch_scripts\db_backup.bat'])
     print('Database dumped. Sleeping for 2 hours then taking another dump')
     time.sleep(7200)
