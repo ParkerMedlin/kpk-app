e73bced4c8907beacf32b91fcc6601fc907ec458
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-10-13 14:53:05 -0500

    Rename data_update and smooth out monitor refresh.

--- local_machine_scripts/python_db_scripts/app_monitor/monitor.py
+++ local_machine_scripts/python_db_scripts/app_monitor/monitor.py
@@ -2,7 +2,7 @@ import time
 import curses
 from curses import wrapper
 from curses import init_pair
-from table_draw import data_loop,initial_imports
+from table_draw import data_update,initial_imports
 
 
 
@@ -11,15 +11,13 @@ def monitor_activity(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_YELLOW)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
-    imports_window = initial_imports.draw_import_table(stdscr)
-    initial_imports.populate_import_table(imports_window)
+    
         
     while True:
         time.sleep(.5)
-        
-        data_loop_window = data_loop.draw_dataloop_table(stdscr)
-        data_loop.populate_dataloop_table(data_loop_window)
-
-        
+        data_loop_window = data_update.draw_dataloop_table(stdscr)
+        imports_window = initial_imports.draw_import_table(stdscr)
+        initial_imports.populate_import_table(imports_window)
+        data_update.populate_dataloop_table(data_loop_window)
 
 wrapper(monitor_activity)
