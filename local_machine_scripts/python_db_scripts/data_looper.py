d993d9bd30bc282d4296ac4ba179e5e17d44cb49
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-10-13 14:01:52 -0500

    Package the db_mgmt scripts and update monitor.

--- local_machine_scripts/python_db_scripts/data_looper.py
+++ local_machine_scripts/python_db_scripts/data_looper.py
@@ -1,9 +1,10 @@
 import time
-from prod_sched_to_postgres import get_prod_schedule
-import table_builder as f_tables
-import chem_locations_to_postgres as f_chem_locations
-import horix_sched_to_postgres as f_horix_schedule
-import sage_to_postgres as f_sage
+from app_db_mgmt import prod_sched_to_postgres as prod_sched_pg
+from app_db_mgmt import sage_to_postgres as sage_pg
+from app_db_mgmt import horix_sched_to_postgres as horix_pg
+from app_db_mgmt import chem_locations_to_postgres as chem_loc_pg
+from app_db_mgmt import table_builder as calc_tables_pg
+
 from multiprocessing import Process
 
 def update_xlsb_tables():
@@ -11,10 +12,10 @@ def update_xlsb_tables():
         for attempt in range(10):
             try:
                 while True:
-                    get_prod_schedule()
-                    f_tables.create_tables()
-                    f_chem_locations.get_chem_locations()
-                    f_horix_schedule.get_horix_line_blends()
+                    prod_sched_pg.get_prod_schedule()
+                    calc_tables_pg.create_tables()
+                    chem_loc_pg.get_chem_locations()
+                    horix_pg.get_horix_line_blends()
                     print('oh boy here I go again')
             except:
                 print("well well well, looks like we need to take a breaky wakey")
@@ -27,31 +28,31 @@ def update_xlsb_tables():
 
 def get_bmbilldetail():
     while True:
-            f_sage.get_sage_table('BM_BillDetail')
+        sage_pg.get_sage_table('BM_BillDetail')
 
 def get_bmbillheader():
     while True:
-        f_sage.get_sage_table('BM_BillHeader')
+        sage_pg.get_sage_table('BM_BillHeader')
 
 def get_ciitem():
     while True:
-        f_sage.get_sage_table('CI_Item')
+        sage_pg.get_sage_table('CI_Item')
 
 def get_imitemcost():
     while True:
-        f_sage.get_sage_table('IM_ItemCost')
+        sage_pg.get_sage_table('IM_ItemCost')
 
 def get_imitemtransactionhistory():
     while True:
-        f_sage.get_sage_table('IM_ItemTransactionHistory')
+        sage_pg.get_sage_table('IM_ItemTransactionHistory')
 
 def get_imitemwarehouse():
     while True:
-        f_sage.get_sage_table('IM_ItemWarehouse')
+        sage_pg.get_sage_table('IM_ItemWarehouse')
 
 def get_popurchaseorderdetail():
     while True:
-        f_sage.get_sage_table('PO_PurchaseOrderDetail')
+        sage_pg.get_sage_table('PO_PurchaseOrderDetail')
 
 if __name__ == '__main__':
     Process(target=update_xlsb_tables).start()
