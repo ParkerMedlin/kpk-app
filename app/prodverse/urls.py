c40389d0caf73fdad7dae525330f2c2bc9ea9d4b
BLENDVERSE SERVER <me@parkermedl.in>
2022-10-20 09:40:35 -0500

    implement search by description and error handling

--- app/prodverse/urls.py
+++ app/prodverse/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from core.views import display_batch_issue_table
-from prodverse.views import display_lookup_item, get_json_item_info
+from prodverse.views import display_lookup_item, get_json_item_info, get_json_item_from_desc
 
 urlpatterns = [
     path('batchIssueTable/<line>', display_batch_issue_table, name='batchIssueTable'),
     path('lookupitem/', display_lookup_item, name='lookup_item'),
     path('lookupitem/iteminfo_request/', get_json_item_info, name='get-item-info'),
+    path('lookupitem/iteminfo_fromdesc_request/', get_json_item_from_desc, name='get-info-from-desc')
 ]
