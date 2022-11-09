345615dbe26ac31f4704b4c08d1ac50cfccb5593
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-11-09 13:43:09 -0600

    Add data-id to tanklevel table rows.

--- app/kpklauncher/urls.py
+++ app/kpklauncher/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from kpklauncher import views
 
 urlpatterns = [
-    path("launcher/", views.launcher, name="launcher")   
+    path("launcher/", views.launcher, name="launcher")
 ]
