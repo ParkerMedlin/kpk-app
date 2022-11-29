19262666a4f9d9eb9d7065e92bcb3b115206f02f
Parker Medlin <pmedlinkinpakinc@gmail.com>
2022-11-29 10:33:16 -0600

    Implement email button on finished count report.

--- local_machine_scripts/batch_scripts/import_commands.bat
+++ local_machine_scripts/batch_scripts/import_commands.bat
@@ -1,5 +1,3 @@
-docker exec kpk-app_app_1 sh -c "python manage.py import_batches --path /db_imports/lotnums.csv"
-ping -n 2 127.0.0.1 >NUL
 docker exec kpk-app_app_1 sh -c "python manage.py import_instructions --path /db_imports/blendinstructions.csv"
 ping -n 2 127.0.0.1 >NUL
 docker exec kpk-app_app_1 sh -c "python manage.py import_foamfactor --path /db_imports/foamfactor.csv"
