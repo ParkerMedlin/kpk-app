9613cf7dcfa868be3950ae6e8ef8e50e5bd14a92
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-01-10 06:10:01 -0600

    Update batchfile paths to use %USERPROFILE%.

--- local_machine_scripts/batch_scripts/critical_tables_backup/dump_core_deskoneschedule.bat
+++ local_machine_scripts/batch_scripts/critical_tables_backup/dump_core_deskoneschedule.bat
@@ -1,1 +1,1 @@
-docker exec -t kpk-app_db_1 pg_dump --table="public.core_deskoneschedule" -d blendversedb -U postgres > "C:\Users\pmedl\Desktop\core_deskoneschedule_dump.sql"
+docker exec -t kpk-app_db_1 pg_dump --table="public.core_deskoneschedule" -d blendversedb -U postgres > "%USERPROFILE%\Desktop\core_deskoneschedule_dump.sql"
--- local_machine_scripts/batch_scripts/critical_tables_backup/dump_core_foamfactor.bat
+++ local_machine_scripts/batch_scripts/critical_tables_backup/dump_core_foamfactor.bat
@@ -1,1 +1,1 @@
-docker exec -t kpk-app_db_1 pg_dump --table="public.core_foamfactor" -d blendversedb -U postgres > "C:\Users\pmedl\Desktop\core_foamfactor_dump.sql"
+docker exec -t kpk-app_db_1 pg_dump --table="public.core_foamfactor" -d blendversedb -U postgres > "%USERPROFILE%\Desktop\core_foamfactor_dump.sql"
