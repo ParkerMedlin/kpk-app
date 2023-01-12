3d4d16d6ede38ee89a704192babec562e3abfd44
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-01-12 06:30:29 -0600

    Improve blend part number mouseovers.

--- app/core/views.py
+++ app/core/views.py
@@ -105,6 +105,10 @@ def display_blend_these(request):
     desk_one_queryset = DeskOneSchedule.objects.all()
     desk_two_queryset = DeskTwoSchedule.objects.all()
     for blend in blend_these_queryset:
+        this_blend_bom = BlendBillOfMaterials.objects.filter(bill_no__iexact=blend.blend_pn)
+        blend.ingredients_list = f'Sage OH for blend {blend.blend_pn}:\n{str(round(blend.qtyonhand, 0))} gal \n\nINGREDIENTS:\n'
+        for item in this_blend_bom:
+            blend.ingredients_list += item.component_itemcode + ': ' + item.component_desc + '\n'
         if blend.last_txn_date > blend.last_count_date:
             blend.needs_count = True
         else:
@@ -833,6 +837,11 @@ def display_count_report(request, encoded_pk_list):
 
 def display_all_upcoming_production(request):
     upcoming_runs_queryset = TimetableRunData.objects.order_by('starttime')
+    for blend in upcoming_runs_queryset:
+        this_blend_bom = BlendBillOfMaterials.objects.filter(bill_no__iexact=blend.blend_pn)
+        blend.ingredients_list = f'Ingredients for blend {blend.blend_pn}:\n'
+        for item in this_blend_bom:
+            blend.ingredients_list += item.component_itemcode + ': ' + item.component_desc + '\n'
     upcoming_runs_paginator = Paginator(upcoming_runs_queryset, 25)
     page_num = request.GET.get('page')
     current_page = upcoming_runs_paginator.get_page(page_num)
