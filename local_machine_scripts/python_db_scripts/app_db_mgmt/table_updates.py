b0a9fc211689cfb5c89c00f7717f527796d69c25
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-01-24 09:00:55 -0600

    Rectify forklift email & base template errors.

--- local_machine_scripts/python_db_scripts/app_db_mgmt/table_updates.py
+++ local_machine_scripts/python_db_scripts/app_db_mgmt/table_updates.py
@@ -11,10 +11,27 @@ def update_lot_number_sage():
         f.write('Checking sage for lot numbers...')
     connection_postgres = psycopg2.connect('postgresql://postgres:blend2021@localhost:5432/blendversedb')
     cursor_postgres = connection_postgres.cursor()
-    cursor_postgres.execute('''update core_lotnumrecord set sage_qty_on_hand=(select im_itemcost.quantityonhand from im_itemcost
-	    where im_itemcost.receiptno=core_lotnumrecord.lot_number limit 1)''')
-    cursor_postgres.execute('''update core_lotnumrecord set sage_entered_date=(select im_itemcost.transactiondate from im_itemcost 
-	    where im_itemcost.receiptno=core_lotnumrecord.lot_number limit 1)''')
+    cursor_postgres.execute('''WITH subquery AS (
+                SELECT core_lotnumrecord.part_number,
+                    MAX(im_itemcost.quantityonhand) AS max_quantityonhand
+                FROM core_lotnumrecord
+                LEFT JOIN im_itemcost
+                ON core_lotnumrecord.part_number = im_itemcost.itemcode 
+                GROUP BY core_lotnumrecord.part_number
+                )
+                UPDATE core_lotnumrecord
+                SET sage_qty_on_hand = im_itemcost.quantityonhand,
+                    sage_entered_date = im_itemcost.transactiondate
+                FROM subquery
+                LEFT JOIN im_itemcost
+                ON subquery.part_number = im_itemcost.itemcode
+                AND subquery.max_quantityonhand = im_itemcost.quantityonhand
+                '''
+                )
+    # cursor_postgres.execute('''update core_lotnumrecord set sage_qty_on_hand=(select im_itemcost.quantityonhand from im_itemcost
+	#     where im_itemcost.receiptno=core_lotnumrecord.lot_number limit 1)''')
+    # cursor_postgres.execute('''update core_lotnumrecord set sage_entered_date=(select im_itemcost.transactiondate from im_itemcost 
+	#     where im_itemcost.receiptno=core_lotnumrecord.lot_number limit 1)''')
     connection_postgres.commit()
     cursor_postgres.close()
     connection_postgres.close()
