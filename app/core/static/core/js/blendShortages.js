40aee58a17a000edb57bedd5f1fae9f80bd5d2da
BLENDVERSE SERVER <me@parkermedl.in>
2023-02-01 12:14:41 -0600

    Clarify names in blendshortages.

--- app/core/static/core/js/blendShortages.js
+++ app/core/static/core/js/blendShortages.js
@@ -9,9 +9,9 @@ const $lotNumCells = $('.lotNumCell');
 $(document).ready(function(){
     $lotNumCells.each(function(){
         $(this).click(function(event) {
-            $('#id_item_code').val(event.target.getAttribute('data-itemcode'));
-            $('#id_item_description').val(event.target.getAttribute('data-desc'));
-            $('#id_lot_quantity').val(
+            $('#id_newLotModal-item_code').val(event.target.getAttribute('data-itemcode'));
+            $('#id_newLotModal-item_description').val(event.target.getAttribute('data-desc'));
+            $('#id_newLotModal-lot_quantity').val(
                 Math.round(parseFloat(event.target.getAttribute('data-threewkqty')))
                 );
         });
