ebc6af534e3ec888d271ec36f2214c3dd9339dfe
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-02-08 10:55:31 -0600

    Update the workings of new lot number submission.

--- app/core/static/core/js/pageModules/blendSchedule.js
+++ app/core/static/core/js/pageModules/blendSchedule.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
     $('.lotNumButton').each(function(){
         $(this).click(thisAddLotNumModal.setAddLotModalInputs);
     });
+    const urlParameters = new URLSearchParams(window.location.search);
+    let blendArea = urlParameters.get('blendarea');
+    thisAddLotNumModal.formElement.prop("action", `/core/addlotnumrecord/blendschedule?=${blendArea}`);
 });
