479e396468f8183e32f920358ed5a695f1e74da9
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-02-09 12:02:08 -0600

    Correct syntax error.

--- app/prodverse/urls.py
+++ app/prodverse/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from prodverse.views import *
+from prodverse import views
 
 urlpatterns = [
-    path('productionschedule/', display_production_schedule, name='excel_inline'),
-    path('specsheet/<str:item_code>/', display_specsheet_detail, name='specsheet_detail'),
-    path('specsheet/specsheet-error', display_specsheet_error_page, name='specsheet_error_page'),
-    path('productionschedule/', display_production_schedule, name='excel_inline'),
+    path('productionschedule/', views.display_production_schedule, name='excel_inline'),
+    path('specsheet/<str:item_code>/', views.display_specsheet_detail, name='specsheet_detail'),
+    path('specsheet/specsheet-error', views.display_specsheet_error_page, name='specsheet_error_page'),
+    path('productionschedule/', views.display_production_schedule, name='excel_inline')
+]
