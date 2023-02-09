47bbfb72a43f3cf7827f3914f9e1b225b9714ece
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-02-09 11:31:56 -0600

    Resolve item description issue on blendshortages.

--- app/core/views.py
+++ app/core/views.py
@@ -49,7 +49,7 @@ def display_blend_these(request):
         this_blend_bom = BillOfMaterials.objects.filter(item_code__iexact=blend.component_item_code)
         blend.ingredients_list = f'Sage OH for blend {blend.component_item_code}:\n{str(round(blend.qtyonhand, 0))} gal \n\nINGREDIENTS:\n'
         for item in this_blend_bom:
-            blend.ingredients_list += item.component_item_code + ': ' + item.item_description + '\n'
+            blend.ingredients_list += item.component_item_code + ': ' + item.component_item_description + '\n'
         if blend.last_txn_date and blend.last_count_date:
             if blend.last_txn_date > blend.last_count_date:
                 blend.needs_count = True
