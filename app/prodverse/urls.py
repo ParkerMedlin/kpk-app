e7adbf69a7ada23d38d575a85bc4f5cc0167dd46
Jordan Davis <jdavis@kinpakinc.com>
2023-02-14 10:11:34 -0600

    Add dropdowns to prod sch, add specsheets and specsheet lookup pages

--- app/prodverse/urls.py
+++ app/prodverse/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from prodverse import views
 
 urlpatterns = [
-    path('productionschedule/', views.display_production_schedule, name='excel_inline'),
+    path('pickticket/<str:item_code>/', views.display_pickticket_detail, name='specsheet_detail'),
+    path('productionschedule/', views.display_production_schedule, name='production_schedule'),
     path('specsheet/<str:item_code>/', views.display_specsheet_detail, name='specsheet_detail'),
-    path('specsheet/specsheet-error', views.display_specsheet_error_page, name='specsheet_error_page'),
-    path('productionschedule/', views.display_production_schedule, name='excel_inline')
+    path('specsheet/specsheet-lookup', views.display_specsheet_lookup_page, name='specsheet_error_page'),
 ]
