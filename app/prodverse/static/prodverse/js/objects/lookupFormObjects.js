4e68f10d47b0c28720b89f491646e79172659462
BLENDVERSE SERVER <me@parkermedl.in>
2023-02-14 12:04:05 -0600

    Update navbar & js objects.

---
+++ app/prodverse/static/prodverse/js/objects/lookupFormObjects.js
@@ -0,0 +1,109 @@
+export class SpecsheetLookupForm {
+    constructor() {
+        try {
+            this.setUpAutofill();
+            console.log("Instance of class SpecsheetLookupForm created.");
+        } catch(err) {
+            console.error(err.message);
+        }
+    };
+
+    BOMFields = getAllBOMFields('specsheet-items');
+    itemQuantityDiv = $("#item_quantity");
+
+    setModalButtonLink(itemData) {
+        $("#id_specsheet_button_link").attr("href", `/prodverse/specsheet/${itemData.item_code}`);
+    }
+
+    setItemQuantityDiv(itemData) {
+        let qtyOnHand = Math.round(itemData.qtyOnHand, 0)
+        $("#item_quantity").text(`${qtyOnHand} ${itemData.standardUOM}`)
+    };
+    
+    setFields(itemData){
+        $("#id_item_code").val(itemData.item_code);
+        $("#id_item_description").val(itemData.item_description);
+    };
+
+    setUpAutofill() {
+        let BOMFields = getAllBOMFields('specsheet-items');
+        let setFields = this.setFields;
+        let setItemQuantityDiv = this.setItemQuantityDiv;
+        let setModalButtonLink = this.setModalButtonLink;
+        try {
+            $( function() {
+                // ===============  Item Number Search  ==============
+                $("#id_item_code").autocomplete({ // Sets up a dropdown for the part number field 
+                    minLength: 2,
+                    autoFocus: true,
+                    source: function (request, response) {
+                        let results = $.ui.autocomplete.filter(BOMFields.item_codes, request.term);
+                        response(results.slice(0,10));
+                    },
+                    change: function(event, ui) { // Autofill desc when change event happens to the item_code field 
+                        indicateLoading("itemCode");
+                        let itemCode;
+                        if (ui.item==null) { // in case the user clicks outside the input instead of using dropdown
+                            itemCode = $("#id_item_code").val();
+                        } else {
+                            itemCode = ui.item.label.toUpperCase();
+                        }
+                        let itemData = getItemInfo(itemCode, "itemCode");
+                        setFields(itemData);
+                        setItemQuantityDiv(itemData);
+                        setModalButtonLink(itemData);
+                    },
+                    select: function(event , ui) { // Autofill desc when select event happens to the item_code field 
+                        indicateLoading();
+                        let itemCode = ui.item.label.toUpperCase(); // Make sure the item_code field is uppercase
+                        let itemData = getItemInfo(itemCode, "itemCode");
+                        setFields(itemData);
+                        setItemQuantityDiv(itemData);
+                        setModalButtonLink(itemData);
+                    },
+                });
+                //   ===============  Description Search  ===============
+                $("#id_item_description").autocomplete({ // Sets up a dropdown for the part number field 
+                    minLength: 3,
+                    autoFocus: true,
+                    source: function (request, response) {
+                        let results = $.ui.autocomplete.filter(BOMFields.item_descriptions, request.term);
+                        response(results.slice(0,300));
+                    },
+                    change: function(event, ui) { // Autofill desc when change event happens to the item_code field 
+                        indicateLoading("itemDescription");
+                        let itemDesc;
+                        if (ui.item==null) { // in case the user clicks outside the input instead of using dropdown
+                            itemDesc = $("#id_item_description").val();
+                        } else {
+                            itemDesc = ui.item.label.toUpperCase();
+                        }
+                        let itemData = getItemInfo(itemDesc, "itemDescription");
+                        setFields(itemData);
+                        setItemQuantityDiv(itemData);
+                        setModalButtonLink(itemData);
+                    },
+                    select: function(event , ui) { // Autofill desc when select event happens to the item_code field 
+                        indicateLoading();
+                        let itemDesc = ui.item.label.toUpperCase(); // Make sure the item_code field is uppercase
+                        let itemData = getItemInfo(itemDesc, "itemDescription");
+                        setFields(itemData);
+                        setItemQuantityDiv(itemData);
+                        setModalButtonLink(itemData);
+                    },
+                });
+            });
+        } catch (err) {
+            console.error(err.message);
+        };
+        $('#id_item_code').focus(function(){
+            $('.animation').hide();
+            $("#warningParagraph").hide();
+        }); 
+        $("#id_item_description").focus(function(){
+            $('.animation').hide();
+            $("#warningParagraph").hide();
+        });
+    };
+
+}
--- app/prodverse/urls.py
+++ app/prodverse/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from prodverse import views
-from core import views
+from prodverse import views as prodverse_views
+from core import views as core_views
 
 urlpatterns = [
-    path('pickticket/<str:item_code>/', views.display_pickticket_detail, name='specsheet_detail'),
-    path('productionschedule/', views.display_production_schedule, name='production_schedule'),
-    path('specsheet/<str:item_code>/', views.display_specsheet_detail, name='specsheet_detail'),
-    path('specsheet/specsheet-lookup', views.display_specsheet_lookup_page, name='specsheet_error_page'),
-    path('lookupitemquantity/', core.views.display_lookup_item_quantity, name='core_lookup_item'),
+    path('pickticket/<str:item_code>/', prodverse_views.display_pickticket_detail, name='specsheet_detail'),
+    path('productionschedule/', prodverse_views.display_production_schedule, name='production_schedule'),
+    path('specsheet/<str:item_code>/', prodverse_views.display_specsheet_detail, name='specsheet_detail'),
+    path('specsheet-lookup/', prodverse_views.display_specsheet_lookup_page, name='specsheet_error_page'),
+    path('lookupitemquantity/', core_views.display_lookup_item_quantity, name='core_lookup_item'),
 ]
