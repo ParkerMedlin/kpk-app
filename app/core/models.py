c6d7c82d8f0cfad992e48dc1203524b8564d2a13
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-03-06 08:40:43 -0600

    Add po_number filtering to component shortages.

--- app/core/models.py
+++ app/core/models.py
@@ -371,6 +371,7 @@ class ComponentShortage(models.Model):
     two_wk_short = models.DecimalField(max_digits=100, decimal_places=2, null=True)
     three_wk_short = models.DecimalField(max_digits=100, decimal_places=2, null=True)
     unscheduled_short = models.DecimalField(max_digits=100, decimal_places=2, null=True)
+    next_order_due = models.DateField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -417,6 +418,7 @@ class SubComponentShortage(models.Model):
     component_item_description = models.TextField(blank=True, null=True)
     subcomponent_item_code = models.TextField(blank=True, null=True)
     standard_uom = models.TextField(blank=True, null=True)
+    next_order_due = models.DateField(blank=True, null=True)
 
     class Meta:
         managed = False
