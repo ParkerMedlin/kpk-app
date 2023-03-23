5856eac92d3f881bb98d27f2aa50827d0abcc40b
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-03-23 13:00:36 -0500

    Refine issuesheet table function.

--- local_machine_scripts/python_db_scripts/tester.py
+++ local_machine_scripts/python_db_scripts/tester.py
@@ -3,7 +3,7 @@ from app_db_mgmt import sage_to_postgres as sage_pg
 from app_db_mgmt import horix_sched_to_postgres as horix_pg
 from app_db_mgmt import table_builder as calc_tables_pg
 from app_db_mgmt import table_updates as update_tables_pg
-
+import time
 
 # sage_pg.get_sage_table('IM_ItemCost')
 # table_list = ['BM_BillHeader', 'BM_BillDetail', 'CI_Item', 'IM_ItemWarehouse', 'IM_ItemCost', 'IM_ItemTransactionHistory', 'PO_PurchaseOrderDetail']
@@ -13,10 +13,10 @@ from app_db_mgmt import table_updates as update_tables_pg
 # prod_sched_pg.get_foam_factor()
 # prod_sched_pg.get_starbrite_item_quantities()
 # calc_tables_pg.create_bill_of_materials_table()
-calc_tables_pg.create_component_usage_table()
-calc_tables_pg.create_component_shortages_table()
-calc_tables_pg.create_blend_subcomponent_usage_table()
-calc_tables_pg.create_blend_subcomponent_shortage_table()
+# calc_tables_pg.create_component_usage_table()
+# calc_tables_pg.create_component_shortages_table()
+# calc_tables_pg.create_blend_subcomponent_usage_table()
+# calc_tables_pg.create_blend_subcomponent_shortage_table()
 # calc_tables_pg.create_blend_run_data_table()
 # calc_tables_pg.create_timetable_run_data_table()
 # calc_tables_pg.create_issuesheet_needed_table()
@@ -26,3 +26,20 @@ calc_tables_pg.create_blend_subcomponent_shortage_table()
 # calc_tables_pg.create_weekly_blend_totals_table()
 # horix_pg.get_horix_line_blends()
 # update_tables_pg.update_lot_number_sage()
+
+### TIME COMPARISON ###
+print('function1 go')
+start = time.time()
+# calc_tables_pg.create_component_usage_table()
+calc_tables_pg.newtt()
+end = time.time()
+print('time elapsed: ' + str(end - start))
+print('function2 go')
+start = time.time()
+# calc_tables_pg.create_blend_run_data_table()
+# calc_tables_pg.create_timetable_run_data_table()
+calc_tables_pg.create_issuesheet_needed_table()
+end = time.time()
+print('time elapsed: ' + str(end - start))
+
+
