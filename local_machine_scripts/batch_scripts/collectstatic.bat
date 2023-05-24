d422fb8282bd44bf17e2284a2da169ed6486d84f
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-05-24 09:40:25 -0500

    Env vars in batch scripts.

--- local_machine_scripts/batch_scripts/collectstatic.bat
+++ local_machine_scripts/batch_scripts/collectstatic.bat
@@ -1,1 +1,11 @@
-docker exec kpk-app_app_1 sh -c "python manage.py collectstatic --noinput"
+@echo OFF
+setlocal EnableDelayedExpansion
+
+for /f "tokens=1* delims==" %%a in (C:\Users\pmedlin\Documents\kpk-app\.env) do (
+    set "%%a=%%b"
+)
+
+echo %ACTIVE_APP_CONTAINER%
+
+@echo ON
+docker exec ACTIVE_APP_CONTAINER sh -c "python manage.py collectstatic --noinput"
--- local_machine_scripts/batch_scripts/import_commands.bat
+++
@@ -1,9 +0,0 @@
-docker exec kpk-app_app_1 sh -c "python manage.py import_instructions --path /db_imports/blendinstructions.csv"
-ping -n 2 127.0.0.1 >NUL
-docker exec kpk-app_app_1 sh -c "python manage.py import_foamfactor --path /db_imports/foamfactor.csv"
-ping -n 2 127.0.0.1 >NUL
-docker exec kpk-app_app_1 sh -c "python manage.py import_forklifts --path /db_imports/forkliftinfo.csv"
-ping -n 2 127.0.0.1 >NUL
-docker exec kpk-app_app_1 sh -c "python manage.py import_countrecords --path /db_imports/blndschcounts.csv"
-ping -n 2 127.0.0.1 >NUL
-docker exec kpk-app_app_1 sh -c "python manage.py import_tanks --path /db_imports/storagetanks.csv"
