28877d997fc7c218557d803d65d937fd369fc364
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-06-05 09:45:31 -0500

    Revise issueSheet gen criteria.

--- local_machine_scripts/batch_scripts/collectstatic.bat
+++ local_machine_scripts/batch_scripts/collectstatic.bat
@@ -1,11 +1,2 @@
-@echo OFF
-setlocal EnableDelayedExpansion
-
-for /f "tokens=1* delims==" %%a in (C:\Users\pmedlin\Documents\kpk-app\.env) do (
-    set "%%a=%%b"
-)
-
-echo %ACTIVE_APP_CONTAINER%
-
-@echo ON
-docker exec ACTIVE_APP_CONTAINER sh -c "python manage.py collectstatic --noinput"
+docker exec kpk-app_app_green_1 sh -c "python manage.py collectstatic --noinput"
+docker exec kpk-app_app_blue_1 sh -c "python manage.py collectstatic --noinput"
