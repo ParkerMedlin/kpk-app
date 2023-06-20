5441f5c758569536aa3006de3e365e12f7c02f38
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-06-20 06:55:22 -0500

    Modify loopstatus to mark rows red after 6 mins.

---
+++ app/core/static/core/js/pageModules/loopStatus.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    let currentTime = new Date();
+    const tableRows = document.querySelectorAll("tbody tr");
+    tableRows.forEach(function(row) {
+        // Get the timestamp cell value
+        let timestampCell = row.querySelector("td:nth-child(3)").textContent;
+        console.log(timestampCell);
+        let timestamp = new Date(timestampCell);
+        console.log(timestamp);
+        // Calculate the time difference in minutes
+        let timeDifference = (currentTime - timestamp) / (1000 * 60);
+        // Check if the time difference is greater than 6 minutes
+        if (timeDifference > 6) {
+            row.className = "Failure";
+        }
+    });
+});
