ecafd4e23bbc1c82f215ff8cc9c5e1083b934730
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-07-19 06:12:37 -0500

    Add modelform classes for ocmponentcountrecordforms.

---
+++ app/prodverse/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from prodverse.models import *
+
+class ProdComponentCountRecordForm(forms.ModelForm):
+
+    class Meta:
+        model = ProdComponentCountRecord
+        fields = (
+            'item_code',
+            'item_description',
+            'expected_quantity',
+            'counted_quantity',
+            'counted_date',
+            'variance',
+            'counted',
+            'count_type'
+        )
+        widgets = {
+            'item_code' : forms.TextInput(),
+            'item_description' : forms.TextInput(),
+            'count_type' : forms.TextInput(),
+        }
