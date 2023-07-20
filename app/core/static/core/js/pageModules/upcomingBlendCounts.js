9b6cde0855b2087a79a8535080bf0837eb217bef
Parker Medlin <pmedlinkinpakinc@gmail.com>
2023-07-20 10:28:05 -0500

    Insert the ShiftSelectCheckBoxes class into all relevant pages.

--- app/core/static/core/js/pageModules/upcomingBlendCounts.js
+++ app/core/static/core/js/pageModules/upcomingBlendCounts.js
@@ -1,5 +1,7 @@
 import { CreateCountListButton } from '../objects/buttonObjects.js'
+import { ShiftSelectCheckBoxes } from '../objects/pageUtilities.js'
 
 $(document).ready(function() {
     const thisCreateCountListButton = new CreateCountListButton();
+    const thisShiftSelectCheckBoxes = new ShiftSelectCheckBoxes();
 });
