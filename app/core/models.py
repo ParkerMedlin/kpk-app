f2e69f95b4fdde380ef706d6f5cea7a9e94184af
BLENDVERSE SERVER <me@parkermedl.in>
2023-07-25 11:26:57 -0500

    Rectify models iissues.

--- app/core/models.py
+++ app/core/models.py
@@ -167,7 +167,6 @@ class BlendCountRecord(models.Model):
     counted = models.BooleanField(default=False)
     count_type = models.TextField(blank=True, null=True)
     collection_id = models.TextField(blank=True, null=True)
-    counted_by = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.item_code + "; " + str(self.counted_date)
@@ -182,7 +181,6 @@ class BlendComponentCountRecord(models.Model):
     counted = models.BooleanField(default=False)
     count_type = models.TextField(blank=True, null=True)
     collection_id = models.TextField(blank=True, null=True)
-    counted_by = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.item_code + "; " + str(self.counted_date)
