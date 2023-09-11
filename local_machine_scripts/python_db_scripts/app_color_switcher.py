e4e3311c28c5ed6283771029aad3ee1f7649c008
Parker Medlin <me@parkermedl.in>
2023-09-11 08:30:49 -0500

    Add GUI to app_color_switcher.

--- local_machine_scripts/python_db_scripts/app_color_switcher.py
+++ local_machine_scripts/python_db_scripts/app_color_switcher.py
@@ -1,5 +1,7 @@
 import os
 import time
+import tkinter as tk
+from tkinter import messagebox
 
 def check_file_for_string(file_path, search_string):
     try:
@@ -24,28 +26,45 @@ def replace_string_in_file(file_path, old_string, new_string):
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
 
-nginx_conf_path = os.path.expanduser('~\\Documents\\kpk-app\\nginx\\nginx.conf')
-os.system(f"docker cp kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf {nginx_conf_path}")
+def copy_from_container():
+    nginx_conf_path = os.path.expanduser('~\\Documents\\kpk-app\\nginx\\temp_copy\\nginx.conf')
+    os.system(f"docker cp kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf {nginx_conf_path}")
 
-if check_file_for_string(nginx_conf_path, "server app_blue:8001;"):
-    print(f"The nginx.conf file is pointing to the blue container.\nSwitching to green.")
-    replace_string_in_file(nginx_conf_path, "server app_blue:8001;","server app_green:8002;")
-    os.system(f"docker cp {nginx_conf_path} kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf")
-    # copy the file to the desktop as well for testing
-    os.system(f"docker cp kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf {os.path.expanduser('~/Desktop/kpk-app')}")
-    os.system("docker exec kpk-app_nginx_1 nginx -s reload")
-    # os.remove(nginx_conf_path)
-    print(f"Restarting...")
-    os.system("docker restart kpk-app_app_blue_1")
-    # os.system(f"docker cp kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf {nginx_conf_path}")
+    if check_file_for_string(nginx_conf_path, "server app_blue:8001;"):
+        print(f"The nginx.conf file is pointing to the blue container.\nSwitching to green.")
+        replace_string_in_file(nginx_conf_path, "server app_blue:8001;","server app_green:8002;")
+        os.system(f"docker cp {nginx_conf_path} kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf")
+        os.system("docker exec kpk-app_nginx_1 nginx -s reload")
+        # os.remove(nginx_conf_path)
+        print(f"Restarting the blue container...")
+        os.system("docker restart kpk-app_app_blue_1")
 
-elif check_file_for_string(nginx_conf_path, "server app_green:8002;"):
-    print(f"The nginx.conf file is pointing to the green container.\nSwitching to blue.")
-    replace_string_in_file(nginx_conf_path, "server app_green:8002;","server app_blue:8001;")
+    elif check_file_for_string(nginx_conf_path, "server app_green:8002;"):
+        print(f"The nginx.conf file is pointing to the green container.\nSwitching to blue.")
+        replace_string_in_file(nginx_conf_path, "server app_green:8002;","server app_blue:8001;")
+        os.system(f"docker cp {nginx_conf_path} kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf")
+        os.system("docker exec kpk-app_nginx_1 nginx -s reload")
+        # os.remove(nginx_conf_path)
+        print(f"Restarting the green container...")
+        os.system("docker restart kpk-app_app_green_1")
+
+def copy_from_filesystem():
+    nginx_conf_path = os.path.expanduser('~\\Documents\\kpk-app\\nginx\\nginx.conf')
     os.system(f"docker cp {nginx_conf_path} kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf")
-    # copy the file to the desktop as well for testing
-    os.system(f"docker cp kpk-app_nginx_1:/etc/nginx/conf.d/nginx.conf {os.path.expanduser('~/Desktop/kpk-app/nginx/nginx.conf')}")
-    os.system("docker exec kpk-app_nginx_1 nginx -s reload")
-    # os.remove(nginx_conf_path)
-    print(f"Restarting...")
-    os.system("docker restart kpk-app_app_green_1")
+
+
+def main():
+    root = tk.Tk()
+    root.geometry("300x200")
+    root.title("")
+
+    button1 = tk.Button(root, text="Copy from Container", command=copy_from_container)
+    button1.pack(pady=20)
+
+    button2 = tk.Button(root, text="Copy from Filesystem", command=copy_from_filesystem)
+    button2.pack(pady=20)
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
