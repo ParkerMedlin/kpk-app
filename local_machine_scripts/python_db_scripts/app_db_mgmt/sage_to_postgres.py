1d2279d3845eb6597ce00090bef641a22cd762ed
Parker Medlin <me@parkermedl.in>
2023-09-14 09:14:06 -0500

    Update sagefunctions.

--- local_machine_scripts/python_db_scripts/app_db_mgmt/sage_to_postgres.py
+++ local_machine_scripts/python_db_scripts/app_db_mgmt/sage_to_postgres.py
@@ -158,4 +158,84 @@ def get_sage_table(table_name):
 #     print(f'{dt.datetime.now()} -- blends_produced table created.')
     
 
+def get_all_transactions():
+    try:
+        table_name = "IM_ItemTransactionHistory"
+        print('waiting...')
+        with open(os.path.expanduser('~\\Documents\\kpk-app\\local_machine_scripts\\python_db_scripts\\last_touch\\' + table_name + '_last_update.txt'), 'w', encoding="utf-8") as f:
+            f.write('Pulling from Sage...')
+        csv_path = os.path.expanduser('~\\Documents') + '\\kpk-app\\db_imports\\' + table_name+'.csv'
+        columns_with_types_path = os.path.expanduser('~\\Documents') + '\\kpk-app\\db_imports\\sql_columns_with_types\\' + table_name + '.txt'
 
+        connection_MAS90 = pyodbc.connect("DSN=SOTAMAS90;UID=parker;PWD=Blend2021;",autocommit=True)
+        cursor_MAS90 = connection_MAS90.cursor()
+        cursor_MAS90.execute("SELECT * FROM " + table_name)
+        table_contents = list(cursor_MAS90.fetchall())
+        data_headers = cursor_MAS90.description
+
+        ### maybe someday look at this one with a critical eye
+        sql_columns_with_types = '(id serial primary key, '
+        listPos = 0
+        iter = 0
+        for iter in range(len(data_headers)):
+            sql_columns_with_types = sql_columns_with_types + data_headers[listPos][0]
+            if str(data_headers[listPos][1]) == "<class 'str'>":
+                sql_columns_with_types = sql_columns_with_types + ' text, '
+            elif str(data_headers[listPos][1]) == "<class 'datetime.date'>":
+                sql_columns_with_types = sql_columns_with_types + ' date, '
+            elif str(data_headers[listPos][1]) == "<class 'decimal.Decimal'>":
+                sql_columns_with_types = sql_columns_with_types + ' decimal, '
+            listPos += 1
+        sql_columns_with_types = sql_columns_with_types[:len(sql_columns_with_types)-2] + ')'
+        column_names_only_string = ''
+
+        with open(columns_with_types_path, 'w', encoding="utf-8") as f:
+            f.write(sql_columns_with_types)
+
+        qiter = 0
+        for qiter in range(len(data_headers)):
+            column_names_only_string = column_names_only_string + data_headers[qiter][0] + ', '
+        column_names_only_string = column_names_only_string[:len(column_names_only_string)-2]
+        column_list = column_names_only_string.split(",")
+        cursor_MAS90.close()
+        connection_MAS90.close()
+
+        table_dataframe = pd.DataFrame.from_records(table_contents, index=None, exclude=None, columns=column_list, coerce_float=False, nrows=None)
+        table_dataframe.to_csv(path_or_buf=csv_path, header=column_list, encoding='utf-8')
+
+        with open(columns_with_types_path, encoding="utf-8") as file:
+            sql_columns_list = file.readlines()
+        sql_columns_with_types = sql_columns_list[0]
+
+        connection_postgres = psycopg2.connect('postgresql://postgres:blend2021@localhost:5432/blendversedb')
+        cursor_postgres = connection_postgres.cursor()
+        cursor_postgres.execute("drop table if exists " + table_name + "_TEMP")
+        cursor_postgres.execute("create table " + table_name + "_TEMP" + sql_columns_with_types)
+        copy_sql = "copy " + table_name + "_TEMP from stdin with csv header delimiter as ','"
+        with open(csv_path, 'r', encoding='utf-8') as f:
+            cursor_postgres.copy_expert(sql=copy_sql, file=f)
+        # SET UP INDEXES!
+        cursor_postgres.execute("""DROP INDEX if exists im_itemtxnhist_itemcode_idx;
+                                    CREATE INDEX im_itemtxnhist_itemcode_idx ON im_itemtransactionhistory_TEMP (itemcode);
+                                    DROP INDEX if exists im_itemtxnhist_transactiondate_idx;
+                                    CREATE INDEX im_itemtxnhist_transactiondate_idx ON im_itemtransactionhistory_TEMP (transactiondate);
+                                    DROP INDEX if exists im_itemtxnhist_transactioncode_idx;
+                                    CREATE INDEX im_itemtxnhist_transactioncode_idx ON im_itemtransactionhistory_TEMP (transactioncode);
+                                    DROP INDEX if exists im_itemtxnhist_transactionqty_idx;
+                                    CREATE INDEX im_itemtxnhist_transactionqty_idx ON im_itemtransactionhistory_TEMP (transactionqty);""")
+        cursor_postgres.execute("drop table if exists " + table_name)
+        cursor_postgres.execute("alter table " + table_name + "_TEMP rename to " + table_name)
+        connection_postgres.commit()
+        cursor_postgres.close()
+        connection_postgres.close()
+        print(f'{dt.datetime.now()} -- {table_name} table cloned.')
+
+        return table_name
+    
+    except Exception as e:
+        with open(os.path.expanduser('~\\Documents\\kpk-app\\local_machine_scripts\\python_db_scripts\\last_touch\\' + table_name + '_last_update.txt'), 'w', encoding="utf-8") as f:
+                f.write('SAGE ERROR: ' + str(dt.datetime.now()))
+        with open(os.path.expanduser('~\\Documents\\kpk-app\\local_machine_scripts\\python_db_scripts\\error_logs\\' + table_name + '_error_log.txt'), 'a', encoding="utf-8") as f:
+            f.write('SAGE ERROR: ' + str(dt.datetime.now()))
+            f.write('\n')
+            f.write(str(e))
