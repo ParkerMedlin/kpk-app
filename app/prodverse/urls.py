bc11cfbcab6925214cb576c2a43964fdaafef0f6
Jordan Davis <jdavis@kinpakinc.com>
2023-10-10 14:47:13 -0500

    add components to save and track row state on schd

--- app/prodverse/urls.py
+++ app/prodverse/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('production-schedule/', prodverse_views.display_production_schedule, name='production-schedule'),
     path('spec-sheet/<str:item_code>/<str:po_number>/<str:juliandate>/', prodverse_views.display_specsheet_detail, name='spec-sheet-detail'),
     path('spec-sheet-lookup/', prodverse_views.display_specsheet_lookup_page, name='spec-sheet-error-page'),
-    path('lookup-item-quantity/', core_views.display_lookup_item_quantity, name='core-lookup-item')
+    path('lookup-item-quantity/', core_views.display_lookup_item_quantity, name='core-lookup-item'),
+    path('update-checkbox-state/<int:row_id>', views.update_checkbox_state, name='update_checkbox_state'),
+    path('update-dropdown-state/<int:row_id>', views.update_dropdown_state, name='update_dropdown_state'),
+    path('get-row-state/<int:row_id>', views.get_row_state, name='get_row_state'),
 ]
