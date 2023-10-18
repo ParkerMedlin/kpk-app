56acbbc24222676da63eb1399139f46fc3b4fc97
Parker Medlin <me@parkermedl.in>
2023-10-18 07:24:29 -0500

    Update issue sheet functionality.

--- app/prodverse/static/prodverse/js/pageModules/productionSchedule.js
+++ app/prodverse/static/prodverse/js/pageModules/productionSchedule.js
@@ -1,7 +1,5 @@
 import { ProductionSchedulePage } from '../objects/pageObjects.js'
-import { IssueSheetDropdown } from '../objects/buttonObjects.js'
 
 $(document).ready(function(){
     const thisProductionSchedulePage = new ProductionSchedulePage();
-    const thisIssueSheetDropdown = new IssueSheetDropdown();
 });
