2450698b79c045c8f1ff95149fd26e383d035cdc
Jordan Davis <jdavis@kinpakinc.com>
2023-10-19 06:40:47 -0500

    add app deployment 1-step file

---
+++ local_machine_scripts/batch_scripts/app_deploy.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Starting Docker...
+start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+
+:loop
+timeout /t 1 >nul
+docker info >nul 2>&1
+if errorlevel 1 (
+    echo Waiting for Docker to start...
+    goto loop
+)
+
+echo Docker started.
+
+echo Running the app...
+cd C:/Users/jdavis/Documents/kpk-app
+docker-compose -p kpk-app -f docker-compose-PROD.yml up -d
+
+echo Restoring the latest backup...
+call db_restore_latest_backup.bat
+
+echo Updating the database...
+cd %USERPROFILE%/Documents/kpk-app
+python %USERPROFILE%/Documents/kpk-app/local_machine_scripts/python_db_scripts/data_looper.py
