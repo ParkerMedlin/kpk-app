99191247b17096d50068f2a688b673e89e83da31
Parker Medlin <me@parkermedl.in>
2023-11-14 11:56:43 -0600

    Change foam_factor field back to factor.

--- app/core/forms.py
+++ app/core/forms.py
@@ -137,12 +137,12 @@ class LotNumRecordForm(forms.ModelForm):
 class FoamFactorForm(forms.ModelForm):
     class Meta:
         model = FoamFactor
-        fields = ('item_code','item_description','foam_factor')
+        fields = ('item_code','item_description','factor')
 
         widgets = {
             'item_code' : forms.TextInput(),
             'item_description' : forms.TextInput(),
-            'foam_factor' : forms.NumberInput(attrs={'pattern': '[0-9]*'})
+            'factor' : forms.NumberInput(attrs={'pattern': '[0-9]*'})
         }
 
 item_location_blend_objects = ItemLocation.objects.filter(item_type__iexact='blend')
