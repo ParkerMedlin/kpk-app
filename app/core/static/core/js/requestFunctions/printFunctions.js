21674869fbbbdf03dc1fba7c212e6576b3aea863
Parker Medlin <me@parkermedl.in>
2023-12-20 06:51:39 -0600

    Updoot

--- app/core/static/core/js/requestFunctions/printFunctions.js
+++ app/core/static/core/js/requestFunctions/printFunctions.js
@@ -1,11 +1,22 @@
-export  function printBlendLabel(imageData){
-    let response;
-    let printURL = `/core/print-blend-label?imageData=${imageData}`;
-    $.post(printURL,{"imageData": imageData}, function(response) {
-        if (response.status == 'Success') {
-            console.log('Label printed successfully.');
-        } else {
-            alert('Failed to print label: ' + response.message);
-        }
+export async function sendImageToServer(imageB64String, deviceJson, optionsJson) {
+    // Convert the base64 string to a Blob
+    let fetchResponse = await fetch(imageB64String);
+    let blob = await fetchResponse.blob();
+    formData.append('blob', blob, 'printImage.jpg');
+
+    // Create a FormData object and append the Blob
+    let formData = new FormData();
+    formData.append('blob', blob, 'filename.jpg');
+
+    // Send the FormData object to your Django server
+    let response = await fetch('/core/print-blend-label/', {
+        method: 'POST',
+        body: formData
     });
-}; 
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+        console.log('Image sent successfully');
+    }
+}
