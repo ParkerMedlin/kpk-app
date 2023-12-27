fb3f41c46d8140df31f9989e92ddad88349d6d8c
Parker Medlin <me@parkermedl.in>
2023-12-27 09:10:35 -0600

    Begin implementing a label button on the production schedule.

--- app/core/static/core/js/pageModules/blendlabeltemplate.js
+++ app/core/static/core/js/pageModules/blendlabeltemplate.js
@@ -1,7 +1,7 @@
 import { ZebraPrintButton } from '../objects/buttonObjects.js';
 
 $(document).ready(function(){
-    const thisZebraPrintButton = new ZebraPrintButton(document.getElementById("printButton"));
+    const thisZebraPrintButton = new ZebraPrintButton(document.getElementById("blendLabelPrintButton"));
 });
 
 
