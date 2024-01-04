5a711c6e0d0d9d1867e888c94cabb920669af27f
Parker Medlin <me@parkermedl.in>
2024-01-04 11:11:52 -0600

    Push delete function and finalize styling on editor.

--- app/core/static/core/js/pageModules/blendInstructionEditor.js
+++ app/core/static/core/js/pageModules/blendInstructionEditor.js
@@ -1,5 +1,17 @@
 import { BlendInstructionEditorPage } from '../objects/pageObjects.js'
+import { getURLParameter } from '../requestFunctions/requestFunctions.js'
 
 $(document).ready(function() {
     const thisBlendInstructionEditorPage = new BlendInstructionEditorPage();
+    const deleteButtons = document.querySelectorAll('.deleteBtn');
+
+    deleteButtons.forEach(function(button) {
+        button.addEventListener('click', function(e){
+            document.getElementById('deleteDialog').showModal();
+            let confirmDeleteButton = document.getElementById('confirmDelete');
+            let objectId = e.currentTarget.getAttribute('data-item-id');
+            let encodedItemCode = getURLParameter('itemCode')
+            confirmDeleteButton.setAttribute('href', `/core/delete-blend-instruction?objectID=${objectId}&encodedItemCode=${encodedItemCode}`)
+        })
+    })
 });
