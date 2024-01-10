746668cecc85da0f481cba28d41089ae3f605a15
Parker Medlin <park.medlin007@gmail.com>
2024-01-10 07:44:51 -0600

    Change tank level logging to include a row for each tank.

--- app/core/models.py
+++ app/core/models.py
@@ -884,7 +884,6 @@ class TankLevelLog(models.Model):
     height_capacity_inches = models.DecimalField(max_digits=50, decimal_places=2, blank=True)
     filled_gallons = models.DecimalField(max_digits=50, decimal_places=2, blank=True)
 
-
     def __str__(self):
         return self.timestamp
 
