156eede405b95f7675c1fbacb2d5d9acfc587ec2
BLENDVERSE SERVER <me@parkermedl.in>
2024-02-01 08:45:33 -0600

    Update blendshortages styling and attributes.

--- app/core/views.py
+++ app/core/views.py
@@ -90,7 +90,7 @@ def display_blend_shortages(request):
     advance_blends = ['602602','602037','602037EUR','93700.B','94700.B','93800.B']
     for blend in blend_shortages_queryset:
         if blend.component_item_code in advance_blends:
-            blend_shortages_queryset.advance_blend = 'yes'
+            blend.advance_blend = 'yes'
     component_item_codes = blend_shortages_queryset.values_list('component_item_code', flat=True)
 
     foam_factor_is_populated = FoamFactor.objects.all().exists()
