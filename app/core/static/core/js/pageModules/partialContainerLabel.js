046d1c6f067ae6bc16b92269c430b7ea7f3bbbb6
Parker Medlin <park.medlin007@gmail.com>
2024-03-04 13:34:48 -0600

    Publish html template and js file.

---
+++ app/core/static/core/js/pageModules/partialContainerLabel.js
@@ -0,0 +1,9 @@
+import { ZebraPrintButton } from '../objects/buttonObjects.js';
+
+$(document).ready(function(){
+    const thisZebraPrintButton = new ZebraPrintButton(document.getElementById("blendLabelPrintButton"));
+    $("#label-lot-number-dropdown").change(function(e) {
+        let selectedOptionValue = this.value;
+        $("#blend-label-lot-number").text(selectedOptionValue);
+    });
+});
