d92be75b5177f571d64c25e32dfb921100c20b8c
Parker Medlin <park.medlin007@gmail.com>
2024-03-20 07:40:17 -0500

    Revise dataloop tests.

--- local_machine_scripts/python_db_scripts/tester.py
+++ local_machine_scripts/python_db_scripts/tester.py
@@ -1,11 +1,11 @@
 import time
 import pandas as pd
 from app_db_mgmt import prod_sched_to_postgres as prod_sched_pg
-from app_db_mgmt import sage_to_postgres as sage_pg
+# from app_db_mgmt import sage_to_postgres as sage_pg
 from app_db_mgmt import horix_sched_to_postgres as horix_pg
 from app_db_mgmt import table_builder as calc_tables_pg
 from app_db_mgmt import table_updates as update_tables_pg
-from app_db_mgmt import xtendo_transactum as long_transactions
+# from app_db_mgmt import xtendo_transactum as long_transactions
 from app_db_mgmt import i_eat_the_specsheet as specsheet_eat
 
 
@@ -15,7 +15,7 @@ from app_db_mgmt import i_eat_the_specsheet as specsheet_eat
 # sage_pg.get_sage_table('IM_ItemTransactionHistory')
 # long_transactions.get_sage_table('IM_ItemTransactionHistory')
 
-sage_pg.get_sage_table('PO_PurchaseOrderHeader')
+# sage_pg.get_sage_table('PO_PurchaseOrderHeader')
 # table_list = ['BM_BillHeader', 'BM_BillDetail', 'CI_Item', 'IM_ItemWarehouse', 'IM_ItemCost', 'IM_ItemTransactionHistory', 'PO_PurchaseOrderDetail']
 # for item in table_list:
 #     sage_pg.get_sage_table(item)
@@ -42,26 +42,26 @@ sage_pg.get_sage_table('PO_PurchaseOrderHeader')
 # specsheet_eat.get_spec_sheet()
 # update_tables_pg.update_lot_number_sage()
 
-# all_functions = {
-    # 'create_bill_of_materials_table' : calc_tables_pg.create_bill_of_materials_table,
-    # 'get_prod_schedule' : prod_sched_pg.get_prod_schedule,
-    # 'get_foam_factor' : prod_sched_pg.get_foam_factor,
-    # 'get_starbrite_item_quantities' : prod_sched_pg.get_starbrite_item_quantities,
-    # 'create_component_usage_table' : calc_tables_pg.create_component_usage_table,
-    # 'create_component_shortages_table' : calc_tables_pg.create_component_shortages_table,
-    # 'create_blend_subcomponent_usage_table' : calc_tables_pg.create_blend_subcomponent_usage_table,
-    # 'create_blend_subcomponent_shortage_table' : calc_tables_pg.create_blend_subcomponent_shortage_table,
-    # 'create_blend_run_data_table' : calc_tables_pg.create_blend_run_data_table,
-    # 'create_timetable_run_data_table' : calc_tables_pg.create_timetable_run_data_table,
-    # 'create_blendthese_table' : calc_tables_pg.create_blendthese_table,
-    # 'create_upcoming_blend_count_table' : calc_tables_pg.create_upcoming_blend_count_table,
-    # 'create_upcoming_component_count_table' : calc_tables_pg.create_upcoming_component_count_table,
-    # 'create_weekly_blend_totals_table' : calc_tables_pg.create_weekly_blend_totals_table,
-    # 'create_adjustment_statistic_table' : calc_tables_pg.create_adjustment_statistic_table,
-    # 'get_horix_line_blends' : horix_pg.get_horix_line_blends,
-    # 'update_lot_number_sage' : update_tables_pg.update_lot_number_sage
+all_functions = {
+    'create_bill_of_materials_table' : calc_tables_pg.create_bill_of_materials_table,
+    'get_prod_schedule' : prod_sched_pg.get_prod_schedule,
+    'get_foam_factor' : prod_sched_pg.get_foam_factor,
+    'get_starbrite_item_quantities' : prod_sched_pg.get_starbrite_item_quantities,
+    'create_component_usage_table' : calc_tables_pg.create_component_usage_table,
+    'create_component_shortages_table' : calc_tables_pg.create_component_shortages_table,
+    'create_blend_subcomponent_usage_table' : calc_tables_pg.create_blend_subcomponent_usage_table,
+    'create_blend_subcomponent_shortage_table' : calc_tables_pg.create_blend_subcomponent_shortage_table,
+    'create_blend_run_data_table' : calc_tables_pg.create_blend_run_data_table,
+    'create_timetable_run_data_table' : calc_tables_pg.create_timetable_run_data_table,
+    'create_blendthese_table' : calc_tables_pg.create_blendthese_table,
+    'create_upcoming_blend_count_table' : calc_tables_pg.create_upcoming_blend_count_table,
+    'create_upcoming_component_count_table' : calc_tables_pg.create_upcoming_component_count_table,
+    'create_weekly_blend_totals_table' : calc_tables_pg.create_weekly_blend_totals_table,
+    'create_adjustment_statistic_table' : calc_tables_pg.create_adjustment_statistic_table,
+    'get_horix_line_blends' : horix_pg.get_horix_line_blends,
+    'update_lot_number_sage' : update_tables_pg.update_lot_number_sage
     # 'check_hashes' : sage_pg.check_hashes
-# }
+}
 
 
 
@@ -82,4 +82,21 @@ sage_pg.get_sage_table('PO_PurchaseOrderHeader')
 # end = time.time()
 # print('time elapsed: ' + str(end - start))
 
+# Initialize a list to store function names and execution times
+timing_data = []
 
+# Iterate over all functions in the all_functions dictionary
+for func_name, func in all_functions.items():
+    start_time = time.time()  # Capture start time
+    func()  # Execute the function
+    end_time = time.time()  # Capture end time
+    execution_time = end_time - start_time  # Calculate execution time
+    
+    # Append the function name and its execution time as a dictionary to the list
+    timing_data.append({'Function Name': func_name, 'Execution Time': execution_time})
+
+# Convert the list of dictionaries to a DataFrame
+timing_df = pd.DataFrame(timing_data)
+
+# Print the DataFrame to display the function names and their execution times
+print(timing_df)
