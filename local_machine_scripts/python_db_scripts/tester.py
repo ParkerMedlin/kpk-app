2815dda489b87981bbf104e81265b6b842925085
Parker Medlin <park.medlin007@gmail.com>
2024-06-19 13:26:26 -0500

    Some db script changes to transactions

--- local_machine_scripts/python_db_scripts/tester.py
+++ local_machine_scripts/python_db_scripts/tester.py
@@ -2,11 +2,11 @@ import time
 # import pandas as pd
 # from app_db_mgmt import prod_sched_to_postgres as prod_sched_pg
 # from app_db_mgmt import sage_to_postgres as sage_pg
-from app_db_mgmt import horix_sched_to_postgres as horix_pg
+# from app_db_mgmt import horix_sched_to_postgres as horix_pg
 # from app_db_mgmt import horix_sched_to_postgres_experimental as horix_pg
 # from app_db_mgmt import table_builder as calc_tables_pg
 # from app_db_mgmt import table_updates as update_tables_pg
-# from app_db_mgmt import xtendo_transactum as long_transactions
+from app_db_mgmt import xtendo_transactum as long_transactions
 # from app_db_mgmt import i_eat_the_specsheet as specsheet_eat
 # from app_db_mgmt import tank_level_reading
 
@@ -15,7 +15,6 @@ from app_db_mgmt import horix_sched_to_postgres as horix_pg
 
 # sage_pg.get_sage_table('CI_Item')
 # sage_pg.get_sage_table('IM_ItemTransactionHistory')
-# long_transactions.get_sage_table('IM_ItemTransactionHistory')
 
 # sage_pg.get_sage_table('IM_ItemTransactionHistory')
 # table_list = ['BM_BillHeader', 'BM_BillDetail', 'CI_Item', 'IM_ItemWarehouse', 'IM_ItemCost', 'IM_ItemTransactionHistory', 'PO_PurchaseOrderDetail']
@@ -44,9 +43,10 @@ from app_db_mgmt import horix_sched_to_postgres as horix_pg
 # update_tables_pg.update_lot_number_sage()
 
 all_functions = {
+    'long_transactions' : long_transactions.get_sage_table
     # 'update_tank_levels_table' : tank_level_reading.update_tank_levels_table,
     # 'get_prod_schedule' : prod_sched_pg.get_prod_schedule,
-    'get_horix_line_blends' : horix_pg.get_horix_line_blends,
+    # 'get_horix_line_blends' : horix_pg.get_horix_line_blends,
     # 'update_desk_values_in_lot_number' : update_tables_pg.update_lot_number_desks,
     # 'get_starbrite_item_quantities' : prod_sched_pg.get_starbrite_item_quantities,
     # 'create_bill_of_materials_table' : calc_tables_pg.create_bill_of_materials_table,
@@ -66,7 +66,7 @@ all_functions = {
 for func_name, func in all_functions.items():
     print(f'{func_name} go')
     start = time.time()
-    func()
+    func('IM_ItemTransactionHistory')
     end = time.time()
     print('time elapsed: ' + str(end - start))
     # Append the results of each function run to the dataframe
