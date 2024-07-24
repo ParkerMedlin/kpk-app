531ca9d2a6416449568324a373be14658471244f
Parker Medlin <park.medlin007@gmail.com>
2024-07-24 10:14:39 -0500

    Update Sage script references.

--- local_machine_scripts/python_db_scripts/app_db_mgmt/xtendo_transactum.py
+++ local_machine_scripts/python_db_scripts/app_db_mgmt/xtendo_transactum.py
@@ -6,18 +6,28 @@ import time
 import os
 import pandas as pd
 import datetime as dt
+from dotenv import load_dotenv
 
 def get_sage_table(table_name):
     print('waiting...')
     csv_path = os.path.expanduser('~\\Documents') + '\\kpk-app\\db_imports\\' + table_name+'.csv'
     columns_with_types_path = os.path.expanduser('~\\Documents') + '\\kpk-app\\db_imports\\sql_columns_with_types\\' + table_name + '.txt'
     try:
-        connection_MAS90 = pyodbc.connect(r"""Driver={MAS 90 4.0 ODBC Driver}; UID=parker; PWD=Blend2024;
-                                                Directory=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90;
-                                                Prefix=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\SY\,
-                                                \\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\==\;
-                                                ViewDLL=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\HOME; Company=KPK;
-                                                LogFile=\PVXODBC.LOG; CacheSize=8; DirtyReads=1; BurstMode=1;
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        env_path = os.path.join(current_dir, '..', '..', '.env')
+        load_dotenv(dotenv_path=env_path)
+
+        SAGE_USER = os.getenv('SAGE_USER')
+        SAGE_PW = os.getenv('SAGE_PW')
+
+        if not SAGE_USER or not SAGE_PW:
+            raise ValueError("Sage credentials not found in environment variables.")
+        connection_MAS90 = pyodbc.connect(r"Driver={MAS 90 4.0 ODBC Driver}; " + f"UID={SAGE_USER}; PWD={SAGE_PW}; " +
+                                                r"""Directory=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90; 
+                                                Prefix=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\SY\, 
+                                                \\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\==\; 
+                                                ViewDLL=\\Kinpak-Svr1\Apps\Sage 100 ERP\MAS90\HOME; Company=KPK; 
+                                                LogFile=\PVXODBC.LOG; CacheSize=8; DirtyReads=1; BurstMode=1; 
                                                 StripTrailingSpaces=1;""", autocommit=True)
     except Error as this_error:
         print('SAGE ERROR: Could not connect to Sage. Please verify that internet is connected and Sage is operational.')
