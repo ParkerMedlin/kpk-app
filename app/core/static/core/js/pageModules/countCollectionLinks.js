2418d9c2603ab64dcec05a0f0ee9e3d8a2f60c47
Parker Medlin <park.medlin007@gmail.com>
2024-10-02 14:18:31 -0500

    Update countlinks page.

--- app/core/static/core/js/pageModules/countCollectionLinks.js
+++ app/core/static/core/js/pageModules/countCollectionLinks.js
@@ -1,7 +1,10 @@
 import { CountCollectionLinksPage } from '../objects/pageObjects.js'
 import { CountCollectionWebSocket } from '../objects/webSocketObjects.js'
+import { AddAutomatedBlendcountButton, AddAutomatedBlendcomponentcountButton } from '../objects/buttonObjects.js'
 
 $(document).ready(function(){
     const thisCountCollectionWebSocket = new CountCollectionWebSocket()
     const thisCountListPage = new CountCollectionLinksPage(thisCountCollectionWebSocket);
+    const thisAddAutomatedBlendcountButton = new AddAutomatedBlendcountButton(document.getElementById('add-automated-blendcount-button'));
+    const thisAddAutomatedBlendcomponentcountButton = new AddAutomatedBlendcomponentcountButton(document.getElementById('add-automated-blendcomponentcount-button'));
 });
