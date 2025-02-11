d18a8cec506f7e7b933bc2a4259eaa496d2d4b36
Parker Medlin <park.medlin007@gmail.com>
2025-02-11 10:12:55 -0600

    Secure initadmin

--- app/app/management/commands/initadmin.py
+++ app/app/management/commands/initadmin.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from django.conf import settings
+import os
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            User.objects.create_superuser("admin", "pmedlin@kinpakinc.com", "blend2021")
+            User.objects.create_superuser(os.environ['DJANGO_SUPERUSER_USERNAME'], "pmedlin@kinpakinc.com", os.environ['DJANGO_SUPERUSER_PASSWORD'])
         else:
             print('Oopsy woopsy')
