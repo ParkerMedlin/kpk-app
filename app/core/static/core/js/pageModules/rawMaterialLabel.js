87ae12e9498bcb891f828c8a191c3b4c6dbbb544
Parker Medlin <park.medlin007@gmail.com>
2025-09-22 13:50:00 -0500

    Resolve Teak Oil row issue on tank levels page.

--- app/core/static/core/js/pageModules/rawMaterialLabel.js
+++ app/core/static/core/js/pageModules/rawMaterialLabel.js
@@ -1,8 +1,10 @@
 import { RawLabelLookupForm } from '../objects/lookupFormObjects.js'
+import { RawMaterialLabelPrintButton } from '../objects/buttonObjects.js'
 // import { RawMaterialLabelPage } from '../objects/pageObjects.js'
 
 $(document).ready(function(){
     const firstRawLabelLookupForm = new RawLabelLookupForm($("#id_item_code_1"),$("#id_item_description_1"),$("#location_field_1"),$("#topUnitsField"));
     const secondRawLabelLookupForm = new RawLabelLookupForm($("#id_item_code_2"),$("#id_item_description_2"),$("#location_field_2"),$("#bottomUnitsField"));
+    const thisRawMaterialLabelPrintButton = new RawMaterialLabelPrintButton(document.getElementById("rawMaterialLabelPrintButton"));
     // const thisRawMaterialLabelPage = new RawMaterialLabelPage();
 });
