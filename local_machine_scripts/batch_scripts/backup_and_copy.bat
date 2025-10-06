c542cadb67f6adab9ad237e2db67957e0a4ac86d
Parker Medlin <park.medlin007@gmail.com>
2025-10-06 12:00:36 -0500

    Update the backup script.

--- local_machine_scripts/batch_scripts/backup_and_copy.bat
+++ local_machine_scripts/batch_scripts/backup_and_copy.bat
@@ -1,8 +1,8 @@
-set backupdir="%date:~-10,2%"-"%date:~7,2%"-"%date:~-4,4%"-"backup"
-mkdir %USERPROFILE%\desktop\%backupdir%
-mkdir %USERPROFILE%\desktop\%backupdir%\full_db
+set backupdir=%date:~-10,2%-%date:~7,2%-%date:~-4,4%-%time:~0,2%-%time:~3,2%-%time:~6,2%-backup
+set backupdir=%backupdir: =0%
 for /f "usebackq tokens=1,* delims==" %%A in (`findstr /b "DB_USER= DB_PASS= DB_NAME=" "%USERPROFILE%\Documents\kpk-app\.env"`) do set "%%A=%%B"
+mkdir "%USERPROFILE%\Desktop\full_db" 2>nul
 docker exec -e PGPASSWORD=%DB_PASS% -i kpk-app_db_1 pg_dump -U %DB_USER% %DB_NAME% > "%USERPROFILE%\Desktop\full_db\full_db_dump.sql"
-mkdir M:\kpkapp\backups\%backupdir%
-ROBOCOPY "%USERPROFILE%\desktop\\%backupdir%" "M:\kpkapp\backups\\%backupdir%" /E
-rmdir /s /q "%USERPROFILE%\desktop\%backupdir%"
+mkdir "M:\kpkapp\backups\%backupdir%" 2>nul
+ROBOCOPY "%USERPROFILE%\Desktop\full_db" "M:\kpkapp\backups\%backupdir%" full_db_dump.sql /E
+rmdir /s /q "%USERPROFILE%\Desktop\full_db" 2>nul
