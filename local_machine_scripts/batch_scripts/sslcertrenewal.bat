9fa39c6edb5b2f7e261f046b295d3d246f82355a
Parker Medlin <park.medlin007@gmail.com>
2025-10-29 10:55:17 -0500

    Update cert references in renewl .bat and systray process.

--- local_machine_scripts/batch_scripts/sslcertrenewal.bat
+++ local_machine_scripts/batch_scripts/sslcertrenewal.bat
@@ -1,20 +1,20 @@
 @echo off
 REM Generate a new Certificate Signing Request (CSR)
-openssl req -new -key nginx/ssl/exceladdin.key -out nginx/ssl/new_exceladdin.csr -config nginx/ssl/openssl.conf
+openssl req -new -key nginx/ssl/kpkapp-rootCA.key -out nginx/ssl/new_kpkapp-rootCA.csr -config nginx/ssl/openssl.conf
 
 REM Self-sign the new certificate
-openssl x509 -req -days 365 -in nginx/ssl/new_exceladdin.csr -signkey nginx/ssl/exceladdin.key -out nginx/ssl/new_exceladdin.crt
+openssl x509 -req -days 365 -in nginx/ssl/new_kpkapp-rootCA.csr -signkey nginx/ssl/kpkapp-rootCA.key -out nginx/ssl/new_kpkapp-rootCA.crt
 
 REM Convert the new certificate to PEM format
-openssl x509 -in nginx/ssl/new_exceladdin.crt -out nginx/ssl/new_exceladdin.pem -outform PEM
+openssl x509 -in nginx/ssl/new_kpkapp-rootCA.crt -out nginx/ssl/new_kpkapp-rootCA.pem -outform PEM
 
 REM Replace the old certificate
-move /Y nginx/ssl/new_exceladdin.crt nginx/ssl/exceladdin.crt
-move /Y nginx/ssl/new_exceladdin.pem nginx/ssl/exceladdin.pem
+move /Y nginx/ssl/new_kpkapp-rootCA.crt nginx/ssl/kpkapp-rootCA.crt
+move /Y nginx/ssl/new_kpkapp-rootCA.pem nginx/ssl/kpkapp-rootCA.pem
 
 REM Copy the new certificate files into the Docker container
-docker cp nginx/ssl/exceladdin.crt kpk-app_nginx_1:/etc/nginx/ssl/
-docker cp nginx/ssl/exceladdin.pem kpk-app_nginx_1:/etc/nginx/ssl/
+docker cp nginx/ssl/kpkapp-rootCA.crt kpk-app_nginx_1:/etc/nginx/ssl/
+docker cp nginx/ssl/kpkapp-rootCA.pem kpk-app_nginx_1:/etc/nginx/ssl/
 
 REM Reload Nginx to apply changes
 docker exec kpk-app_nginx_1 nginx -s reload
