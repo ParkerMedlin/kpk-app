de838ffd663affd45686a7f0d917ae20a77abd08
Parker Medlin <park.medlin007@gmail.com>
2025-11-10 07:49:02 -0600

    Distribute blend_shortages processing.

--- app/core/selectors/production_planning_selectors.py
+++ app/core/selectors/production_planning_selectors.py
@@ -1,6 +1,45 @@
-from core.models import DeskOneSchedule, DeskTwoSchedule, BillOfMaterials
+import logging
+
+from core.models import CiItem, ComponentShortage, DeskOneSchedule, DeskTwoSchedule, BillOfMaterials
 from django.db.models import Q
 from django.http import JsonResponse
+
+logger = logging.getLogger(__name__)
+
+MARINE_LINE_PGAF = [
+    '052004N', '052006', '052070', '32500.B', '32700.B', '32800.B',
+    '33200CONC.B', '33200DIL.B', '33200DILRED.B', '33300.B', '33400.B',
+    '33700.B', '33900.B', '619529-BLUE.B', 'AF600.B', 'ANT1G.B',
+    'ANT1G5050.B', 'ANT27BLUE.B'
+]
+
+
+def get_excluded_blend_item_codes():
+    """Return blend item codes that should always be excluded from shortages."""
+    excluded_qs = (
+        CiItem.objects
+        .filter(productline__in=['W/W', 'PGAF'])
+        .filter(itemcodedesc__startswith='BLEND')
+        .exclude(itemcode__in=MARINE_LINE_PGAF)
+        .values_list('itemcode', flat=True)
+    )
+    excluded_codes = list(excluded_qs)
+    logger.debug('Excluded %s blend item codes from shortages view.', len(excluded_codes))
+    return excluded_codes
+
+
+def get_schedulable_blend_shortages():
+    """Return the base queryset used by the blend shortages dashboard."""
+    excluded_item_codes = get_excluded_blend_item_codes()
+    return (
+        ComponentShortage.objects
+        .filter(component_item_description__startswith='BLEND')
+        .filter(procurement_type__iexact='M')
+        .filter(component_instance_count=1)
+        .exclude(prod_line__iexact='Hx')
+        .exclude(component_item_code__in=excluded_item_codes)
+        .order_by('start_time')
+    )
 
 def get_components_in_use_soon(request):
     """Get list of components that will be used soon in scheduled blends.
