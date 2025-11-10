18ed1e8460f58bd45e249d98979533b584dceda8
Parker Medlin <park.medlin007@gmail.com>
2025-11-10 09:07:16 -0600

    Divvy batch issue sheet logic.

--- app/core/views/web.py
+++ app/core/views/web.py
@@ -38,6 +38,14 @@ from core.kpkapp_utils.string_utils import get_unencoded_item_code
 from core.services.lot_numbers_services import generate_next_lot_number
 from core.services.blend_scheduling_services import get_blend_schedule_querysets, prepare_blend_schedule_queryset
 from django.core.paginator import Paginator
+from core.selectors.batch_issue_selectors import (
+    get_batch_issue_runs,
+    get_positive_lot_numbers,
+)
+from core.services.batch_issue_services import (
+    build_batch_issue_data,
+    resolve_issue_date,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -641,74 +649,18 @@ def display_batch_issue_table(request, prod_line, issue_date):
             - Issue dates and lot numbers
     """
 
-    all_lot_numbers_with_quantity = LotNumRecord.objects.filter(sage_qty_on_hand__gt=0).order_by('sage_entered_date')
-
-    if prod_line == 'all':
-        prod_runs_this_line = ComponentUsage.objects  \
-            .filter(component_item_description__startswith='BLEND') \
-            .filter(start_time__lte=12) \
-            .order_by('start_time')
-    else: 
-        prod_runs_this_line = ComponentUsage.objects  \
-            .filter(component_item_description__startswith='BLEND') \
-            .filter(prod_line__iexact=prod_line) \
-            .filter(start_time__lte=12) \
-            .order_by('start_time')
-
-    for run in prod_runs_this_line:
-        if run.component_onhand_after_run < 0:
-            run.shortage_flag = 'short'
-        elif run.component_onhand_after_run < 25:
-            run.shortage_flag = 'warning'
-        else: 
-            run.shortage_flag = 'noshortage'
-
-    runs_this_line = [] # track which runs have been added to this line
-    if issue_date == "nextDay":
-        tomorrow = dt.date.today() + dt.timedelta(days=1)
-        if tomorrow.weekday() == 4:  # If tomorrow is Friday
-            next_possible_monday = tomorrow + dt.timedelta(days=2)
-            issue_date = next_possible_monday.strftime("%m-%d-%y")
-        else:
-            next_possible_weekday = tomorrow
-            issue_date = next_possible_weekday.strftime("%m-%d-%y")
-    
-    for run in prod_runs_this_line:
-        # skip duplicates
-        check_string = run.component_item_code+run.prod_line 
-        if any((str(d.get('component_item_code', None)+d.get('prod_line', None))) == check_string for d in runs_this_line):
-            continue
-
-        run_dict = {
-            'component_item_code' : run.component_item_code,
-            'component_item_description' : run.component_item_description,
-            'component_on_hand_qty' : run.component_on_hand_qty,
-            'prod_line' : run.prod_line,
-            'issue_date' : issue_date,
-            'shortage_flag' : run.shortage_flag
-        }
-
-        lot_numbers = []
-        for lot_num_record in all_lot_numbers_with_quantity:
-            if lot_num_record.item_code == run.component_item_code:
-                lot_numbers.append( (lot_num_record.lot_number, str(lot_num_record.sage_qty_on_hand)+" gal") )
-        if not run.procurement_type == 'M':
-            lot_numbers.append( ("Purchased", "See QC lab.") )
-        if run.procurement_type == 'M' and not lot_numbers:
-            lot_numbers.append( ("Unavailable", "Check issue sheet page on tablet.") )
-        run_dict['lot_numbers'] = lot_numbers
-        runs_this_line.append(run_dict)
-
-    inline_runs = { 'prod_line' : 'INLINE', 'run_list' : [run for run in runs_this_line if run['prod_line'] == 'INLINE'] }
-    pd_line_runs = { 'prod_line' : 'PD LINE', 'run_list' : [run for run in runs_this_line if run['prod_line'] == 'PD LINE'] }
-    jb_line_runs = { 'prod_line' : 'JB LINE', 'run_list' : [run for run in runs_this_line if run['prod_line'] == 'JB LINE'] }
-    hx_line_runs = { 'prod_line' : 'FOGG', 'run_list' : [run for run in runs_this_line if run['prod_line'] == 'HORIX'] }
-
-    prod_runs_by_line = [inline_runs, pd_line_runs, jb_line_runs]
+    resolved_issue_date = resolve_issue_date(issue_date)
+    lot_numbers_with_qty = get_positive_lot_numbers()
+    prod_runs_this_line = get_batch_issue_runs(prod_line)
+    runs_this_line, prod_runs_by_line = build_batch_issue_data(
+        prod_runs_this_line,
+        lot_numbers_with_qty,
+        resolved_issue_date,
+    )
 
     return render(request, 'core/lotnumbers/batchissuetable.html', {'runs_this_line' : runs_this_line,
                                                          'prod_line' : prod_line,
-                                                         'issue_date' : issue_date,
+                                                         'issue_date' : resolved_issue_date,
                                                          'prod_runs_by_line' : prod_runs_by_line
                                                          })
 
