7040f3c6889c97a3f4b7fd64e26813f47136f79a
Parker Medlin <park.medlin007@gmail.com>
2025-12-01 12:09:36 -0600

    Implement totals and update naming on template.

--- app/core/static/core/js/pageModules/componentCoverage.js
+++ app/core/static/core/js/pageModules/componentCoverage.js
@@ -1,23 +1,5 @@
 import { ComponentCoveragePage } from '../objects/pageObjects.js'
 
 $(document).ready(function() {
-    const coveragePage = new ComponentCoveragePage(window.componentCoverageData || {});
-
-    $('#refreshCoverageBtn').on('click', function() {
-        const $btn = $(this);
-        $btn.prop('disabled', true).text('Refreshing...');
-
-        fetch('/core/component-stock-coverage/data/')
-            .then(response => response.json())
-            .then(data => {
-                coveragePage.updateData(data);
-            })
-            .catch(err => {
-                console.error('Failed to refresh component coverage data', err);
-                alert('Could not refresh right now. Please try again in a moment.');
-            })
-            .finally(() => {
-                $btn.prop('disabled', false).text('Refresh Snapshot');
-            });
-    });
+    new ComponentCoveragePage(window.componentCoverageData || {});
 });
