a27acaffa0dec72e4276df33a85d57ad812f0367
Parker Medlin <park.medlin007@gmail.com>
2025-12-08 09:48:16 -0600

    Hotfix to rebuild move/delete links upon addition to blend schedule.

--- local_machine_scripts/batch_scripts/db_restore.bat
+++ local_machine_scripts/batch_scripts/db_restore.bat
@@ -1,3 +1,3 @@
 @echo off
 for /f "tokens=2 delims==" %%a in ('findstr "DB_PASS=" "%USERPROFILE%\Documents\kpk-app\.env"') do set DB_PASS=%%a
-docker exec -i -e PGPASSWORD=%DB_PASS% kpk-app_db_1 psql -U postgres -d blendversedb < "C:\OD\OneDrive - Kinpak, Inc\Desktop\full_db_dump.sql"
+docker exec -i -e PGPASSWORD=%DB_PASS% kpk-app_db_1 psql -U postgres -d blendversedb < "%USERPROFILE%\Desktop\full_db_dump.sql"
