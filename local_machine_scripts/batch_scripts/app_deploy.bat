f9b78a69c5283098c21c3b9f712396a90d6c9b67
Parker Medlin <park.medlin007@gmail.com>
2025-12-10 13:20:19 -0600

    Expand user options in app_deploy.

--- local_machine_scripts/batch_scripts/app_deploy.bat
+++ local_machine_scripts/batch_scripts/app_deploy.bat
@@ -14,7 +14,21 @@ echo Docker started.
 
 echo Running the app...
 cd %USERPROFILE%/Documents/kpk-app
-docker-compose -p kpk-app -f docker-compose-PROD.yml up -d
+set /p ComposeChoice=Do you want to rebuild images and force recreate containers? (y/n): 
+if /I "%ComposeChoice%" EQU "y" (
+    docker-compose -f docker-compose-PROD.yml -p kpk-app up --build --force-recreate
+) else (
+    docker-compose -p kpk-app -f docker-compose-PROD.yml up -d
+)
+
+set /p UserInput=Do you want to restore the DB and Redis backups? (y/n): 
+if /I "%UserInput%" EQU "y" (
+    echo Restoring the latest DB and Redis backups...
+    cd %USERPROFILE%/Documents/kpk-app/local_machine_scripts/batch_scripts
+    call db_restore_latest_backup.bat
+)
+
+
 
 echo Updating the database...
 cd %USERPROFILE%/Documents/kpk-app
