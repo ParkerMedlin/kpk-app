81c67e66fbd0d3e7efedba9aa248d61839e0b3c6
Parker Medlin <park.medlin007@gmail.com>
2025-12-10 15:13:35 -0600

    Rectify file reference.

--- local_machine_scripts/batch_scripts/app_deploy_from_backup.bat
+++ local_machine_scripts/batch_scripts/app_deploy_from_backup.bat
@@ -19,6 +19,6 @@ docker-compose -p kpk-app -f docker-compose-PROD.yml up -d
 set /p UserInput=Do you want to restore the DB and Redis backups? (y/n): 
 if /I "%UserInput%" EQU "y" (
     echo Restoring the latest DB and Redis backups...
-    cd %USERPROFILE%/Documents/kpk-app/local_machine_scripts/batch_scripts/helpers
+    cd %USERPROFILE%/Documents/kpk-app/local_machine_scripts/batch_scripts/helper_scripts
     call db_restore_latest_backup.bat
 )
