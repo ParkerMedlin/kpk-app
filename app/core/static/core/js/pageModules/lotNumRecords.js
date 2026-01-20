8072c8376655b3f80eeac8d18f6f1c845bf7ba1b
Parker Medlin <park.medlin007@gmail.com>
2026-01-20 11:12:49 -0600

    Update lotnumrecords table utils and add them to lotnums report.

--- app/core/static/core/js/pageModules/lotNumRecords.js
+++ app/core/static/core/js/pageModules/lotNumRecords.js
@@ -3,8 +3,6 @@ import { ShiftSelectCheckBoxes } from '../objects/pageUtilities.js'
 import { CreateCountListButton, GHSSheetGenerator, CreateBlendLabelButton, EditLotNumButton } from '../objects/buttonObjects.js'
 import { BlendScheduleSocket } from '../websockets/blendScheduleSocket.js';
 import { getOrphanedLots } from '../requestFunctions/requestFunctions.js';
-import { initDataTableWithExport } from '../objects/tableObjects.js';
-
 // Notification helper
 function showPrintNotification(type, title, message) {
     const notificationId = `print-notification-${Date.now()}`;
@@ -93,7 +91,11 @@ function enhanceLotNumRecordTableForWebSockets() {
 // --- End functions for data-blend-id ---
 
 $(document).ready(function(){
-    initDataTableWithExport('#lotNumRecordsTable');
+    $('#lotNumRecordsTable').DataTable({
+        paging: false,
+        searching: true,
+        info: false
+    });
 
     const thisShiftSelectCheckBoxes = new ShiftSelectCheckBoxes();
     const thisGHSSheetGenerator = new GHSSheetGenerator();
