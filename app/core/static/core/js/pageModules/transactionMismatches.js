594fde7e6f00836c6e0b3a265c7cc08938e44075
Parker Medlin <park.medlin007@gmail.com>
2026-01-21 10:49:21 -0600

    Correct for convention on transactionmismatches.

--- app/core/static/core/js/pageModules/transactionMismatches.js
+++ app/core/static/core/js/pageModules/transactionMismatches.js
@@ -3,6 +3,9 @@ import { initDataTableWithExport } from '../objects/tableObjects.js';
 $(document).ready(function () {
     initDataTableWithExport('#transactionMismatchesTable', {
         order: [[2, 'desc']],
-        buttons: ['copy', 'csv', 'excel', 'print']
+        buttons: ['copy', 'csv', 'excel', 'print'],
+        columnDefs: [
+            { orderable: false, targets: [3,4,5] }  // 0-based column index
+        ]
     });
 });
