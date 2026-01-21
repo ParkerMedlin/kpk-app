f4209d7a43c68c90799123c6d26348e921e8d05a
Parker Medlin <park.medlin007@gmail.com>
2026-01-21 16:12:18 -0600

    Add the migration to run.

--- app/core/migrations/0001_initial.py
+++ app/core/migrations/0001_initial.py
@@ -1111,23 +1111,6 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='DischargeTestingRecord',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('date', models.DateTimeField(auto_now_add=True, db_index=True)),
-                ('discharge_source', models.CharField(choices=[('JB Line', 'JB Line'), ('INLINE', 'INLINE'), ('PD Line', 'PD Line'), ('Warehouse', 'Warehouse')], max_length=20)),
-                ('flush_type', models.CharField(max_length=100)),
-                ('initial_pH', models.DecimalField(blank=True, decimal_places=2, max_digits=4, null=True)),
-                ('action_required', models.TextField(blank=True, null=True)),
-                ('final_pH', models.DecimalField(blank=True, decimal_places=2, max_digits=4, null=True)),
-                ('final_disposition', models.TextField()),
-            ],
-            options={
-                'db_table': 'core_dischargetestingrecord',
-                'ordering': ['-date', '-id'],
-            },
-        ),
-        migrations.CreateModel(
             name='FoamFactor',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
@@ -1362,16 +1345,6 @@ class Migration(migrations.Migration):
             index=models.Index(fields=['tank_name', '-calculated_at'], name='core_tankco_tank_na_077506_idx'),
         ),
         migrations.AddField(
-            model_name='dischargetestingrecord',
-            name='lab_technician',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='discharge_tests_lab', to=settings.AUTH_USER_MODEL),
-        ),
-        migrations.AddField(
-            model_name='dischargetestingrecord',
-            name='line_personnel',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='discharge_tests_line', to=settings.AUTH_USER_MODEL),
-        ),
-        migrations.AddField(
             model_name='desklaborrate',
             name='updated_by',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
@@ -1395,13 +1368,5 @@ class Migration(migrations.Migration):
             model_name='blendsheet',
             name='lot_number',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.lotnumrecord'),
-        ),
-        migrations.AddIndex(
-            model_name='dischargetestingrecord',
-            index=models.Index(fields=['discharge_source'], name='core_discha_dischar_8759bd_idx'),
-        ),
-        migrations.AddConstraint(
-            model_name='dischargetestingrecord',
-            constraint=models.CheckConstraint(check=models.Q(('final_pH__isnull', True), models.Q(('final_pH__gte', Decimal('5.10')), ('final_pH__lte', Decimal('10.90'))), _connector='OR'), name='discharge_testing_final_ph_range'),
         ),
     ]
